4f2f66eacfe23f14c68cb01ac6fad496fb7c952f
abhishek <abhishekmankame962@gmail.com>
2025-05-28 22:10:38 +0530

    template update

---
+++ template/template.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h> // includes all standard libraries
+using namespace std;
+
+// Type aliases for convenience
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+// Macros for shorthand
+#define fast_io ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+
+// Debugging (can comment out in constents)
+#define debug(x) cerr << #x << ": " << x << endl;
+
+// Constants
+const int MOD = 1e9 + 7; // Modulo value for large numbers
+const int INF = 1e9; // Infinity value for comparisons or (can be set to higher values)
+const int MAXN = 2e5 + 5; // Maximum size for arrays or vectors
