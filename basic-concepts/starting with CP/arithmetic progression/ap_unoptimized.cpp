8c6b9b15ba2c8b047c1da543fd8f412c323aba49
abhishek <abhishekmankame962@gmail.com>
2025-05-28 23:03:50 +0530

    arithmetic progression not optimized code update

---
+++ basic-concepts/starting with CP/arithmetic progression/ap_unoptimized.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int getNthTerm(int a, int n, int d) {
+    int res = a;
+    for(int i = 0; i < n; i++) {
+        res += d;
+    }
+    return res;
+}
+
+int main() {
+    int a, n, d, t;
+    cin>>t;
+    while(t--) {
+        cin >> a >> n >> d;
+        cout << getNthTerm(a, n, d) << "\n";
+    }
+}
