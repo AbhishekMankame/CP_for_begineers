cd1cd27fc1606c59e4b620e828d2b2946a06e84f
abhishek <abhishekmankame962@gmail.com>
2025-05-29 18:16:10 +0530

    common primitive datatypes

--- basic-concepts/starting with CP/data types/common_primitive_datatypes.cpp
+++ basic-concepts/starting with CP/data types/common_primitive_datatypes.cpp
@@ -7,3 +7,14 @@ short d = 30000; // Smaller integer type
 // Character types
 char ch = 'A'; // Single character
 wchar_t wc = L'B'; // Wide character (for unicode)
+
+// Boolean type
+bool e = true; // Boolean value
+
+// Floating-point types
+float f = 3.14f; // Single precision floating-point
+double g = 3.141592653589; // Double precision floating-point
+long double h = 3.14159265358979323846L; // Extended-precision floating-point
+
+// Special type
+void* ptr = nullptr; // void pointer (points to no type)
