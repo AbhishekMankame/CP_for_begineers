df008a97d5b1e543acc7af4467af62f9920eec1b
abhishek <abhishekmankame962@gmail.com>
2025-05-29 18:12:09 +0530

    common primitive datatypes update

---
+++ basic-concepts/starting with CP/data types/common_primitive_datatypes.cpp
@@ -0,0 +1,9 @@
+// Integer types
+int a = 10; // Regular integer
+long long b = 10000000000; // larger integer
+unsigned int c = 42; // Non-negative integer
+short d = 30000; // Smaller integer type
+
+// Character types
+char ch = 'A'; // Single character
+wchar_t wc = L'B'; // Wide character (for unicode)
