285659d9e5fb83f5bf965e71667b0bc8cda8873a
abhishek <abhishekmankame962@gmail.com>
2025-05-31 13:22:05 +0530

    simple_commit

---
+++ basic-concepts/starting with CP/basic_intro.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0); // Fast input/output
+    cin.tie(0); // Unite cin and cout for faster I/O
+
+    cout<< "Hello, World!" << endl; // Output a simple message
+
+    return 0; // Return 0 to indicate successful execution
+}
