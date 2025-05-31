033dbcdae38013a69297c0bfacbc965ee33b73ce
abhishek <abhishekmankame962@gmail.com>
2025-05-31 15:14:52 +0530

    scope example update

--- basic-concepts/input-output.cpp
+++ basic-concepts/input-output.cpp
@@ -16,3 +16,17 @@ In C++, scope is defined by the block in which a variable is declared. Variables
 Conversely, variables declared outside of any function (global variables) can be accessed from anywhere in the program, giving them global scope. 
 Understanding scope is crucial for managing variable lifetimes and avoiding naming conflicts in larger programs. 
 */
+
+// Example of scope in C++
+void OuterFunction() { //Outer Scope
+    int outerVar = 10; // Variable in outer scope
+
+    {
+        int innerVar = 20; // Variable in inner scope
+        cout << "Outer variable inside inner scope: " << outerVar << endl;
+        cout << "Inner variable inside inner scope: " << innerVar << endl;
+    }
+
+    // Trying to access innerVar outside its scope will result in an error
+    // cout << innerVar << endl; // Uncommenting this line will cause a compilation error
+}
