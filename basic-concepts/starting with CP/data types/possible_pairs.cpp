3b4912c40acffab30efa0aaf14b01fbd8157416f
abhishek <abhishekmankame962@gmail.com>
2025-06-01 23:05:19 +0530

    printing pairs

--- basic-concepts/starting with CP/data types/possible_pairs.cpp
+++ basic-concepts/starting with CP/data types/possible_pairs.cpp
@@ -3,3 +3,21 @@ Example:
 Array = {1, 2, 3}
 Result: (1,1), (1,2), (1,3), (2,2), (2,3), (3,3)
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = i; j < n; j++) {
+            cout << "(" << arr[i] << "," << arr[j] << ") "; // Print pairs (i, j)
+        }
+        cout << endl;
+    }
+}
