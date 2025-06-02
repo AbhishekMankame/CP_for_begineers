5a97857bf179f93d75b2f7d1edaac8b1ff7278f2
abhishek <abhishekmankame962@gmail.com>
2025-06-02 12:38:21 +0530

    full pyramid code and output update

--- basic-concepts/starting with CP/pattern printing/full_pyramid.cpp
+++ basic-concepts/starting with CP/pattern printing/full_pyramid.cpp
@@ -15,7 +15,7 @@ Output:
    *****
   *******
  *********
- 
+
 */
 
 #include<iostream>
@@ -24,5 +24,16 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    for(int )
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - i; j++){
+            cout << " ";
+        }
+        for(int j = 0; j <= i; j++ ){
+            cout << "*";
+        }
+        for(int j = 0; j<i; j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
 }
