d81164b356bf2829b25f63812dffa3aa3b977849
abhishek <abhishekmankame962@gmail.com>
2025-06-02 12:33:45 +0530

    full pyramid  question update

---
+++ basic-concepts/starting with CP/pattern printing/full_pyramid.cpp
@@ -0,0 +1,28 @@
+/*Full Pyramid Pattern
+Input: n = 6
+Output:
+     *
+    ***
+   *****
+  *******
+ *********
+***********
+
+Input: n = 5
+Output:
+     *
+    ***
+   *****
+  *******
+ *********
+ 
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int )
+}
