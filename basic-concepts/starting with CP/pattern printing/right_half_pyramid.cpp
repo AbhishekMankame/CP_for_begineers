6b5309e56a60a0c47890005acae1a098aea496d3
abhishek <abhishekmankame962@gmail.com>
2025-06-02 09:47:20 +0530

    right half pyramid

---
+++ basic-concepts/starting with CP/pattern printing/right_half_pyramid.cpp
@@ -0,0 +1,24 @@
+/*Right Half Pyramid
+Input: n = 5
+Output:
+*
+**
+***
+****
+*****
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            cout << "*";
+        }
+        cout <<"\n";
+    }
+}
