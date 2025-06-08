3b9fb8332f35188bd310176ddac1685b803c79f3
abhishek <abhishekmankame962@gmail.com>
2025-06-09 00:01:51 +0530

    time complexity update

--- DSA_problems/mathematics/gcd_using_euclidean1.cpp
+++ DSA_problems/mathematics/gcd_using_euclidean1.cpp
@@ -21,3 +21,8 @@ int main() {
     cin >> a >> b;
     cout << gcd(a, b)<<endl;
 }
+
+// Time Complexity: O(max(a, b))
+// Space Complexity: O(1)
+// The above code uses a repetitive subtraction method to find the GCD of two numbers.
+// This method is not the most efficient, especially for larger numbers, as it can take a long time to converge to the GCD.
