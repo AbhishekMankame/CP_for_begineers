a4169d55edd7f5dc1ff9ab253ef7794e3d631ea9
abhishek <abhishekmankame962@gmail.com>
2025-06-08 22:35:49 +0530

    gcd naive approach function update

---
+++ DSA_problems/mathematics/greatest_common_divisor.cpp
@@ -0,0 +1,18 @@
+/*Greatest Common Divisor*/
+//Naive Approach
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+    int res;
+    int ms=0;
+    for(int i = 1; i <=max(a,b);i++){
+        if(a%i==0 and b%i==0){
+            res=i;
+            ms = max(ms, res);
+        }
+    }
+
+    return ms;
+}
+
