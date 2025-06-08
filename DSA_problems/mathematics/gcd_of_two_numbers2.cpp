2b7536b32015b4366f81fac69f8a6710c56609c2
abhishek <abhishekmankame962@gmail.com>
2025-06-09 00:05:16 +0530

    gcd of two numbers optimized approach

---
+++ DSA_problems/mathematics/gcd_of_two_numbers2.cpp
@@ -0,0 +1,19 @@
+/* GCD of two numbers using optimized approach*/
+
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b){
+    if(b == 0){
+        return a;
+    }
+    else {
+        return gcd(b, a%b);
+    }
+}
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a,b)<<endl;
+}
