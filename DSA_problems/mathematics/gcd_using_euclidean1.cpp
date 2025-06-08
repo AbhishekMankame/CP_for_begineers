fc775d281c5e0a43b059777038efda44224acd5d
abhishek <abhishekmankame962@gmail.com>
2025-06-08 23:58:08 +0530

    gcd of two numbers by repetitive subtraction

---
+++ DSA_problems/mathematics/gcd_using_euclidean1.cpp
@@ -0,0 +1,23 @@
+/*GCD or HCF of two numbers using repetitive subtraction*/
+
+#include<iostream>
+using namespace std;
+
+int gcd(int a, int b){
+    while(a!=b){
+        if(a > b){
+            a = a - b;
+        }
+        else {
+            b = b - a;
+        }
+    }
+
+    return a;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b)<<endl;
+}
