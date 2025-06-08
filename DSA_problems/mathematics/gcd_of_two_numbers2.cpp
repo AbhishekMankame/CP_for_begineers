32afd582e96eec0bdf77ab19591b91c4355af3c2
abhishek <abhishekmankame962@gmail.com>
2025-06-09 00:08:58 +0530

    complexity and other details update

--- DSA_problems/mathematics/gcd_of_two_numbers2.cpp
+++ DSA_problems/mathematics/gcd_of_two_numbers2.cpp
@@ -17,3 +17,9 @@ int main(){
     cin >> a >> b;
     cout << gcd(a,b)<<endl;
 }
+
+// Time Complexity: O(log(min(a, b)))
+// Space Complexity: O(log(min(a, b)))
+// The above code uses the Euclidean algorithm to find the GCD of two numbers.
+// This is a more efficient approach compared to the previous methods, as it reduces the problems size significantly with each recursive call.
+// The major advantage of this approach is that it can handle very large numbers efficiently, making it suitable for competitive programming and real-world applications.
