04cbdb951439dbff8f29aa75a621682ec3fca71b
abhishek <abhishekmankame962@gmail.com>
2025-06-09 13:41:34 +0530

    lcm of two numbers naive approach

---
+++ DSA_problems/mathematics/lcm_of_two_numbers/lcm_naive_approach1.cpp
@@ -0,0 +1,21 @@
+/*LCM of two numbers - Naive Approach*/
+
+#include<iostream>
+using namespace std;
+
+int lcm(int a, int b){
+    int res = max(a,b);
+    while(true){
+        if(res%a==0 and res%b==0){
+            return res;
+        }
+        res++;
+    }
+    return res;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << lcm(a, b) << endl;
+}
