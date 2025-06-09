e4301829a3723cdef4581acaec0961ab9ed81555
abhishek <abhishekmankame962@gmail.com>
2025-06-09 19:52:37 +0530

    is prime optimized approach

---
+++ DSA_problems/mathematics/prime number/prime_number_optimized_approach.cpp
@@ -0,0 +1,17 @@
+/*Checking whether the given number is prime or not - Optimized Approach */
+
+#include<iostream>
+using namespace std;
+
+bool isPrime(int n){
+    if(n==1) return false;
+    if(n==2 or n==3) return true;
+    if(n%2==0 or n%3==0) return false;
+    for(int i = 5; i * i <= n; i = i + 5) {
+        if(n%i == 0 or n%(n+2) == 0){
+            return false;
+        }
+    }
+
+    return true;
+}
