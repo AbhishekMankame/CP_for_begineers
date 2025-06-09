f6b7c94d6035ce048aade72b78872c497a633dde
abhishek <abhishekmankame962@gmail.com>
2025-06-09 23:28:02 +0530

    template update isPrime

--- template/template.cpp
+++ template/template.cpp
@@ -34,6 +34,19 @@ int lcm(int a, int b) {
     return (a*b) / gcd(a,b);
 }
 
+int isPrime(int n){
+    if(n == 1) return false;
+    if(n == 2 || n == 3) return true;
+    if(n%2 == 0 || n%3 == 0) return false;
+    for(int i = 5; i * i <= n; i = i + 5) {
+        if(n%i==0 || n%(i+2) == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 // Solve function (where main logic goes) 
 void solve() {
     int n;
