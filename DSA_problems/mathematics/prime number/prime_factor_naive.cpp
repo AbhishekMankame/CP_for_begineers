efcc6c88af8de67ff9657fa83ecff3d1a0ead4a8
abhishek <abhishekmankame962@gmail.com>
2025-06-09 23:45:03 +0530

    prime factor code update

--- DSA_problems/mathematics/prime number/prime_factor_naive.cpp
+++ DSA_problems/mathematics/prime number/prime_factor_naive.cpp
@@ -15,3 +15,15 @@ int isPrime(int n) {
 
     return true;
 }
+
+void primeFactor(int n) {
+    for(int i = 2; i < n; i++) {
+        if(isPrime(i)){
+            int x = i;
+            while(n%x == 0){
+                cout << i << " ";
+                x = x * i;
+            }
+        }
+    }
+}
