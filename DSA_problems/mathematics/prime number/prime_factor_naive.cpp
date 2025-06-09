7ae055a2c8464addf67430a00d81f93f63763f53
abhishek <abhishekmankame962@gmail.com>
2025-06-09 23:43:23 +0530

    prime factor is prime code update

---
+++ DSA_problems/mathematics/prime number/prime_factor_naive.cpp
@@ -0,0 +1,17 @@
+/*Prime Factor of a number - Naive Approach*/
+
+#include<iostream>
+using namespace std;
+
+int isPrime(int n) {
+    if(n == 1) return false;
+    if(n == 2 || n == 3) return true;
+    if(n%2 == 0 || n%3 == 0) return false;
+    for(int i = 5; i * i <= n; i = i + 5){
+        if(n%i == 0 || n%(i + 2)) {
+            return false;
+        }
+    }
+
+    return true;
+}
