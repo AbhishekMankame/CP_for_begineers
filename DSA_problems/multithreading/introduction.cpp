893de7c649ceab9a1665b61d4a693b00efe296eb
abhishek <abhishekmankame962@gmail.com>
2025-06-11 23:11:22 +0530

    code update

--- DSA_problems/multithreading/introduction.cpp
+++ DSA_problems/multithreading/introduction.cpp
@@ -2,19 +2,25 @@
 #include<thread> // For multithreading
 using namespace std;
 
-void function1() {
-    for(int i = 0; i < 200; i++) {
-        cout << "+";
+void function1(char symbol) {
+    for(int i = 0; i < 20; i++) {
+        cout << symbol;
     }
 }
 
 void function2() {
-    for(int i = 0; i < 200; i++) {
+    for(int i = 0; i < 20; i++) {
         cout << "-";
     }
 }
 
 int main() {
-    function1(); // Call function1 first
-    function2();
+    // function1(); // Call function1 first
+    // function2();
+
+    thread t1(function1,'o'); // Create a thread for function1
+    thread t2(function2); // Create a thread for function2
+
+    system("pause>nul"); // Pause the console to see the output
+
 }
