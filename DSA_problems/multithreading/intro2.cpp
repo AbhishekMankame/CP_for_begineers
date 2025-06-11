e2c95f47ae00f0440d05ce470fe901dce0bf8a3a
abhishek <abhishekmankame962@gmail.com>
2025-06-11 23:28:51 +0530

    thread example

--- DSA_problems/multithreading/intro2.cpp
+++ DSA_problems/multithreading/intro2.cpp
@@ -2,14 +2,18 @@
 #include<thread>
 #include<map>
 #include<string>
+#include<chrono>
 using namespace std;
+using namespace std::chrono_literals;
 
 void RefreshForecast(std::map<std::string, int> forecastMap) {
-    for(auto item: forecastMap) {
-        item.second++;
-        std::cout << item.first<<" - " << item.second<<std::endl;
-    }
-
+    while(true){
+        for(auto& item: forecastMap) {
+            item.second++;
+            std::cout << item.first<<" - " << item.second<<std::endl;
+        }
+        std::this_thread::sleep_for(2000ms);
+    }  
 }
 
 int main() {
@@ -18,7 +22,7 @@ int main() {
         {"Mumbai",20},
         {"Berlin",18}
     };
-
+    std::thread bgWorker(RefreshForecast, forecastMap);
 
     system("pause>nul"); // Pause the console to see the output
 }
