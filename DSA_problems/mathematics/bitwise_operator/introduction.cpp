7c79cbd0dee92bc6533d26d89e95b132c5881ff8
abhishek <abhishekmankame962@gmail.com>
2025-06-11 11:24:29 +0530

    bitwise oprators

---
+++ DSA_problems/mathematics/bitwise_operator/introduction.cpp
@@ -0,0 +1,6 @@
+/*
+Bitwise operators are used to perform operations on bits and are fundamental in low-level programming.
+They allow manipulation of individual bits within an integer, which can lead to efficient algorithms and optimizations.
+Common bitwise operators include AND (&), OR (|), XOR (^), NOT (~), left shift (<<), and right shift (>>).
+
+*/
