52ace45637d21d2e75de9b900187e022c854337a
abhishek <abhishekmankame962@gmail.com>
2025-06-12 13:32:18 +0530

    alternate naive solution

---
+++ DSA_problems/BIT_manipulation/check if Kth bit is set/naive_approach2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void isSet(int n, int k) {
+    for(int i = 0; i < (k - 1); i++) {
+        n = n / 2; // Right shift n by 1 for k - 1 times
+    }
+    if((n & 1) != 0) {
+        cout << "Yes" << endl; // If the Kth bit is set, print "Yes"
+    }
+    else {
+        cout << "No" << endl; // If the Kth bit is not set, print "No"
+    }
+}
+
+int main() {
+    int n, k;
+    cin >> n >> k; // Input the number and the bit position
+    isSet(n, k); // Call the function to check if the Kth bit is set
+    return 0;
+}
