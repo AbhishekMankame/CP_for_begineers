f789128a169e441b19d9efe7bd5543765e67b766
abhishek <abhishekmankame962@gmail.com>
2025-06-13 01:30:45 +0530

    complexity and other details update

--- DSA_problems/BIT_manipulation/count set bits/naive_approach1.cpp
+++ DSA_problems/BIT_manipulation/count set bits/naive_approach1.cpp
@@ -20,3 +20,7 @@ int main() {
 
     return 0;
 }
+
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+// Explanation: The function countBits takes an integer n as input and counts the number of set bits (1s) in its binary representation. It does this by repeatedly checking the least significant bit (LSB) using the modulo operator and then right-shifting the number until it becomes zero. The result is returned as the count of set bits.
