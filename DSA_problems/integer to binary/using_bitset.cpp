1a9c84c70a554bffc526dc19658b60d210929095
abhishek <abhishekmankame962@gmail.com>
2025-06-14 00:32:50 +0530

    output and complexity update

--- DSA_problems/integer to binary/using_bitset.cpp
+++ DSA_problems/integer to binary/using_bitset.cpp
@@ -10,3 +10,6 @@ int main() {
 
     return 0;
 }
+
+// Time Complexity: O(1) // The time complexity is O(1) because the bitset constructor initializes the bitset in constant time for a fixed size (32 bits in this case).
+// Space Complexity: O(1) // The space complexity is O(1) because the bitset size is fixed at 32 bits, regardless of the input value.
