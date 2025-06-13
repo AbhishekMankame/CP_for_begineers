f239d203bc412114c3f38365bcff1d98079571a7
abhishek <abhishekmankame962@gmail.com>
2025-06-14 00:56:16 +0530

    int to bin using recursive approach

--- DSA_problems/integer to binary/using_recursion.cpp
+++ DSA_problems/integer to binary/using_recursion.cpp
@@ -8,9 +8,13 @@ void printBinary(int n) {
 }
 
 int main() {
-    int n  = 10;
+    int n ;
+    cin >> n;
     if(n == 0) cout << 0;
     else printBinary(n);
     cout << endl;
     return 0;
 }
+
+// Time Complexity: O(log n) // The time complexity is O(log n) because the function recursively divides the number by 2, which reduces the problem size logarithmically.
+// Space Complexity: O(log n) // The space complexity is O(log n) due to the recursive call stack, which can go up to log(n) levels deep for the input n.
