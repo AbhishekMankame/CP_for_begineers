4ef3fcd027d9566c905b708839a20957ed1db449
abhishek <abhishekmankame962@gmail.com>
2025-06-19 00:18:59 +0530

    complexity details

--- basic-concepts/Recursion/binary_to_integer.cpp
+++ basic-concepts/Recursion/binary_to_integer.cpp
@@ -20,3 +20,6 @@ int main()
     cout << binaryToDecimal(binary);
     return 0;
 }
+
+// TC: O(n) --> The time complexity is O(n) because we iterate through each character of the binary string exactly once, where n is the length of the binary string.
+// SC: O(1) --> The space complexity is O(1) because we are using a constant amount of space for the variables res, which stores the result, and bit, which is used to iterate through the characthers of the binary string. The input binary string itself does not count towards space compelxity as it is provided by the user and does not grow with the algorithm's execution.
