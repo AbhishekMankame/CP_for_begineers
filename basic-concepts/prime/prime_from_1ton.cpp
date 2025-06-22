25477dc9e04329b080b104435f1dcbc85eca556d
abhishek <abhishekmankame962@gmail.com>
2025-06-22 23:45:32 +0530

    code commit

---
+++ basic-concepts/prime/prime_from_1ton.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void isPrime(int n)
+{
+    bool flag = true;
+    for (int i = 2; i <= n; i++)
+    {
+        for (int j = 2; j * j <= n; j++)
+        {
+            if (n % j == 0)
+            {
+                flag = false;
+            }
+            if (flag == false)
+            {
+                cout << j << " ";
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << isPrime(n);
+    return 0;
+}
