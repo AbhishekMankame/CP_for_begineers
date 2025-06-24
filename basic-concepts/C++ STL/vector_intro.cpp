459fbd93d23bf5b59f9516d383c39b9197fc0c62
abhishek <abhishekmankame962@gmail.com>
2025-06-24 11:57:55 +0530

    simple commit

--- basic-concepts/C++ STL/vector_intro.cpp
+++ basic-concepts/C++ STL/vector_intro.cpp
@@ -4,8 +4,10 @@ using namespace std;
 
 int main()
 {
-    vector<int> vec;        // Declaration of a  vector of integers
-    vec.push_back(10);      // Adding an element to the vector
-    vec.size();             // Returns the number of elements in the vector
-    cout << vec[0] << endl; // Accessing the first element of the vector
+    vector<int> vec;            // Declaration of a  vector of integers
+    vec.push_back(10);          // Adding an element to the vector
+    cout << vec.size() << endl; // Returns the number of elements in the vector
+    cout << vec[0] << endl;     // Accessing the first element of the vector
+    cout << vec.capacity() << endl;
+    ; // Returns the number of elements that can be stored in the vector without reallocating memory
 }
