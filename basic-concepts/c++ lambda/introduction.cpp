06c1015414980822fb8c87751d1057c2af48eedb
abhishek <abhishekmankame962@gmail.com>
2025-06-24 23:58:33 +0530

    some more example update

--- basic-concepts/c++ lambda/introduction.cpp
+++ basic-concepts/c++ lambda/introduction.cpp
@@ -9,5 +9,10 @@ int main()
 
     auto add = [](int a, int b)
     { return a + b; };
-    cout << add(2, 3);
+    cout << add(2, 3) << endl;
+
+    // Generic Lambda
+    auto add1 = [](auto a, auto b)
+    { return a + b; };
+    cout << add1(3.4, 1.5) << endl;
 }
