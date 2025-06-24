99a7ed4ec7109af8f02432d78dfdcfb9c8da14f0
abhishek <abhishekmankame962@gmail.com>
2025-06-25 00:04:34 +0530

    mutable lambda example

--- basic-concepts/c++ lambda/introduction.cpp
+++ basic-concepts/c++ lambda/introduction.cpp
@@ -15,4 +15,11 @@ int main()
     auto add1 = [](auto a, auto b)
     { return a + b; };
     cout << add1(3.4, 1.5) << endl;
+
+    // Mutable Lambda: Mutable lambda allows us to modify those caputerd-by-values variables inside the lambda's body (but only the copy, not the original variable outside).
+    int x = 10;
+    auto f = [x]() mutable
+    {x++; cout << x << endl; };
+    f();
+    cout << x << endl;
 }
