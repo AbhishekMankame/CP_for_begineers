fc4e866b7b3064793ebd4bfcf352688717f594a6
abhishek <abhishekmankame962@gmail.com>
2025-06-25 00:49:10 +0530

    thread ID

--- basic-concepts/c++ lambda/thread_intro.cpp
+++ basic-concepts/c++ lambda/thread_intro.cpp
@@ -11,5 +11,10 @@ int main()
 {
     thread t1(printMessage); // Create a new thread that runs printMessage()
     t1.join();               // Wait for thread to finish
+
+    // Thread ID
+    cout << this_thread::get_id() << endl; // Current thread ID
+    cout << t1.get_id() << endl;           // ID of thread t1
+
     return 0;
 }
