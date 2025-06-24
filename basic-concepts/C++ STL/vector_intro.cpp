5d4b0f1ff52b41149e3ee0930eed003c209cacf9
abhishek <abhishekmankame962@gmail.com>
2025-06-24 12:04:11 +0530

    push back and emplace back update

--- basic-concepts/C++ STL/vector_intro.cpp
+++ basic-concepts/C++ STL/vector_intro.cpp
@@ -14,10 +14,16 @@ int main()
     cout << vec.capacity() << endl;
     ; // Returns the number of elements that can be stored in the vector without reallocating memory
 
+    vec.emplace_back(70); // Adding an element to the end of the vector using emplace_back
+
+    // The emplace_back function constructs an element in place at the end of the vector.
+    // The difference between push back and emplace_back is that push_back copies or moves the element into the vector, emplace_back constructs the elenent in place.
+
     for (int val : vec)
     {
         cout << val << " "; // Iterating through the vector and printing each element
     }
+    cout << endl;
 
     return 0; // Return statement to indicate successful execution
 }
