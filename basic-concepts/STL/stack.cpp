2221a1144f2ace11f37fec7a154a830b3aaf100c
abhishek <abhishekmankame962@gmail.com>
2025-06-26 00:27:19 +0530

    lot of function update

--- basic-concepts/STL/stack.cpp
+++ basic-concepts/STL/stack.cpp
@@ -9,3 +9,37 @@ top() / peek() --> View the top element without removing --> O(1)
 empty() --> Check if stack is empty --> O(1)
 size() --> Get the number of elements --> O(1)
 */
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    stack<int> s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
+    s.push(60);
+    s.push(70);
+    s.push(80);
+
+    cout << "Top element: " << s.top() << endl;
+    s.pop();
+    cout << "Top after pop: " << s.top() << endl;
+    cout << "Size: " << s.size() << endl;
+    cout << "Is empty: " << s.empty() << endl;
+
+    while (!s.empty())
+    {
+        cout << s.top() << " ";
+        s.pop();
+    }
+    cout << endl;
+
+    // cout << "Top element: " << s.top() << endl;
+    cout << "Is empty: " << s.empty() << endl;
+    return 0;
+}
