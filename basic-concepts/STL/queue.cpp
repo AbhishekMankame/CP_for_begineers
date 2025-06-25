99696e135e20d80d8d62e2bfff97db5b97120e17
abhishek <abhishekmankame962@gmail.com>
2025-06-26 00:02:03 +0530

    queue push pop example

--- basic-concepts/STL/queue.cpp
+++ basic-concepts/STL/queue.cpp
@@ -7,11 +7,20 @@ int main()
     queue<int> q;
     q.push(10);
     q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+
     while (!q.empty())
     {
         cout << q.front() << " ";
         q.pop();
     }
+    cout << endl;
+    q.push(60);
+    q.push(70);
+    q.push(80);
+    cout << q.front() << endl;
 
     return 0;
 }
