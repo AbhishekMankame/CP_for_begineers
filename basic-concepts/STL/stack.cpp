6b868ef1c8d319529db19aec0ff3c7215f9ff138
abhishek <abhishekmankame962@gmail.com>
2025-06-26 00:11:35 +0530

    common stack operation

--- basic-concepts/STL/stack.cpp
+++ basic-concepts/STL/stack.cpp
@@ -1,3 +1,11 @@
 // Stack Introduction
 // A stack is a Last-In-First-Out (LIFO) data structure - the last element added is the first one to be removed.
 // Think of it like a stack of plates 🍽️ - you add to the top, and remove from the top.
+
+// Common Operations:
+/*push(x) --> Add x tp the top --> O(1)
+pop() --> Remove the top element --> O(1)
+top() / peek() --> View the top element without removing --> O(1)
+empty() --> Check if stack is empty --> O(1)
+size() --> Get the number of elements --> O(1)
+*/
