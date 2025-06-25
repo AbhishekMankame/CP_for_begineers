6239bcca83465f45accb3df9e13f78908c4b9834
abhishek <abhishekmankame962@gmail.com>
2025-06-26 00:04:52 +0530

    output and code update

--- basic-concepts/STL/queue.cpp
+++ basic-concepts/STL/queue.cpp
@@ -22,5 +22,15 @@ int main()
     q.push(80);
     cout << q.front() << endl;
 
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+
     return 0;
 }
+
+// Time Complexity: for push and pop operations, it is O(1) because it is implemented using a deque (double-ended queue).
+// Space Complexity: O(n) where n is the number of elements in the queue.
+// Note: Queue is FIFO (First In First Out) data structure
