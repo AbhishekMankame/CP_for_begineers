269cb4b90dcb843e5b30d14e23fd2f376e9b0f14
abhishek <abhishekmankame962@gmail.com>
2025-06-26 00:08:38 +0530

    stack introduction

---
+++ basic-concepts/STL/stack.cpp
@@ -0,0 +1,3 @@
+// Stack Introduction
+// A stack is a Last-In-First-Out (LIFO) data structure - the last element added is the first one to be removed.
+// Think of it like a stack of plates 🍽️ - you add to the top, and remove from the top.
