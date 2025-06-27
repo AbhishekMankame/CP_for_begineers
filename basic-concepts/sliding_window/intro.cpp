2978b6bbacd30925c7bf980447e1ee13bbee5e3a
abhishek <abhishekmankame962@gmail.com>
2025-06-27 20:21:14 +0530

    sliding window introduction

---
+++ basic-concepts/sliding_window/intro.cpp
@@ -0,0 +1,8 @@
+// Sliding Window in C++
+/*Sliding window is a technique used to reduce the time complexity of problems involving arrays or strings, especially when dealing with contiguous subarrays or substrings.*/
+
+/* When to Use it?
+- When the problem asks for max/min/sum/average over a subarray of fixed or variable size.
+- Instead of recalculating from scratch every time, we slide a window and reuse previous computations.
+
+*/
