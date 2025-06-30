62676ca8cfe22d108b5b61eebe88745dfad42adc
abhishek <abhishekmankame962@gmail.com>
2025-06-30 21:54:38 +0530

    code and output update

--- bit_manipulation/xor of and - leetcode question/LinkList/introduction.cpp
+++ bit_manipulation/xor of and - leetcode question/LinkList/introduction.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 class Node
 {
+public:
     int data;   // Stores data
     Node *next; // Pointer to the next node
 
@@ -13,3 +14,25 @@ class Node
         next = nullptr;
     }
 };
+
+int main()
+{
+    // Create individual nodes
+    Node *first = new Node(10);
+    Node *second = new Node(20);
+    Node *third = new Node(30);
+
+    // Link nodes
+    first->next = second;
+    second->next = third;
+
+    // Print the linked list
+    Node *temp = first;
+    while (temp != nullptr)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL";
+    return 0;
+}
