e11e11e152770f58c0d83fa2708a002800d48b59
abhishek <abhishekmankame962@gmail.com>
2025-07-04 13:18:23 +0530

    using frequency method

---
+++ basic-concepts/repeating characters in the name/using frequency array.cpp
@@ -0,0 +1,31 @@
+// Here we are using an array of size 256 (ASCII) to store frequencies.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void printRepeatingChars(string str)
+{
+    int freq[256] = {0}; // For all ASCII characters
+
+    for (char ch : str)
+    {
+        freq[(int)ch]++;
+    }
+    for (int i = 0; i < 256; i++)
+    {
+        if (freq[i] > 1)
+        {
+            cout << char(i) << " ";
+        }
+    }
+}
+
+int main()
+{
+    string name;
+    cin >> name;
+    printRepeatingChars(name);
+
+    return 0;
+}
