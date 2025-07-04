b5fbc962f94ecb2db0fe49616075565229fc754e
abhishek <abhishekmankame962@gmail.com>
2025-07-04 21:49:11 +0530

    using set method

---
+++ basic-concepts/repeating characters in the name/using_set.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <unordered_map>
+#include <set>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+
+void printRepeatingCharsOrdered(string str)
+{
+    unordered_map<char, int> freq;
+    set<char> order;
+
+    for (char ch : str)
+    {
+        freq[ch]++;
+        order.insert(ch);
+    }
+
+    for (char ch : str)
+    {
+        if (freq[ch] > 1)
+        {
+            cout << ch << " ";
+            freq[ch] = 0; // Avoid printing again
+        }
+    }
+}
+
+int main()
+{
+    string name = "abhishek";
+    printRepeatingCharsOrdered(name);
+    return 0;
+}
