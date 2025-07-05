50b0c65ce0a366a5cd11a7ed1bd586f3cddf1f3b
abhishek <abhishekmankame962@gmail.com>
2025-07-05 22:14:41 +0530

    strobogrammatic code update

---
+++ basic-concepts/Strobogrammatic Number/246-strobogramamtic.cpp
@@ -0,0 +1,29 @@
+/* LeetCode 246 - Strobogrammatic Number
+
+Given a string `num` which represents a number, return `true` if `num` is a strobogrammatic number.
+*/
+
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+    bool isStrobogrammatic(string num)
+    {
+        unordered_map<char, char> mapping = {
+            {'0', '0'}, {'1', '1'}, {'6', '9'}, {'8', '8'}, {'9', '6'}};
+
+        int left = 0, right = num.size() - 1;
+        while (left <= right)
+        {
+            if (mapping.find(num[left]) == mapping.end())
+                return false;
+
+            left++;
+            right--;
+        }
+        return false;
+    }
+};
