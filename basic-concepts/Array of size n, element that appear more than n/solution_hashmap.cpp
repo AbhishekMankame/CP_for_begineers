94583a4a244a221246aaefb332807f0fa7b0501a
abhishek <abhishekmankame962@gmail.com>
2025-07-06 23:22:10 +0530

    code update

---
+++ basic-concepts/Array of size n, element that appear more than n/solution_hashmap.cpp
@@ -0,0 +1,33 @@
+// Given Array of size n and a number k, find all elements that appear more than n/k times
+/*Input: arr[] = [3, 4, 2, 2, 1, 2, 3, 3], k = 4
+Output: [2, 3]
+Explanation: Here n/k is 8/4 = 2, therefore 2 appears 3 times in the array that is greater than 2 and 3 appears 3 times in the array that is greater than 2
+*/
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+void morethan(vector<int> &arr, int k)
+{
+    int n = arr.size();
+    int x = n / k;
+
+    // unordered_map initialization
+    unordered_map<int, int> freq;
+    for (int i = 0; i < n; i++)
+    {
+        freq[arr[i]]++;
+    }
+
+    // Traversing the map
+    for (auto i : freq)
+    {
+        // Checking if value of key-value pair is greater than x (where x=n/k)
+        if (i.second > x)
+        {
+            cout << i.first << endl;
+        }
+    }
+}
