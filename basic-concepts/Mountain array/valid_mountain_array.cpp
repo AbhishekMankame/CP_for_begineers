e305783552b9c998a7006cfce685dc9cc8d0d637
abhishek <abhishekmankame962@gmail.com>
2025-07-07 16:29:07 +0530

    commit update

--- basic-concepts/Mountain array/valid_mountain_array.cpp
+++ basic-concepts/Mountain array/valid_mountain_array.cpp
@@ -10,3 +10,29 @@ An array arr[] of length n>=3 is called a mountain array if
     -   arr[i] > arr[i+1] ... > arr[n-1] (strictly decreasing after i)
 -   This means arr[i] is the peak of the mountain array
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool validMountainArray(vector<int> &arr)
+    {
+        ios::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(nullptr);
+        int n = arr.size();
+        if (n < 3)
+            return false;
+        int i = 0;
+        while (i + 1 < n && arr[i] < arr[i + 1])
+            i++;
+        if (i == 0 || i = n - 1)
+            return false;
+        while (i + 1 < n && arr[i] >> arr[i + 1])
+            i++;
+
+        return i == n - 1;
+    }
+};
