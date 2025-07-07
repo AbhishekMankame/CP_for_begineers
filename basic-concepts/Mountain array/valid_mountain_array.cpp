aa921322befc7060b7d205c4b1334d5059bbcd3a
abhishek <abhishekmankame962@gmail.com>
2025-07-07 16:20:12 +0530

    mountain array update

---
+++ basic-concepts/Mountain array/valid_mountain_array.cpp
@@ -0,0 +1,5 @@
+/*Leetcode 941 - Valid Mountain Array (Easy)
+Given an array of integers `arr`, return `true` if and only if it is a valid mountain array.
+
+Note: A array is considered as mountain array if array first strictly increases and then strictly decreases - forming a "mountain shape".
+*/
