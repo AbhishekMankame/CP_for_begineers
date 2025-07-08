d7643e2c2611baf69e08e114267cb35bc18d8a67
abhishek <abhishekmankame962@gmail.com>
2025-07-08 23:25:33 +0530

    valid mountain array

--- basic-concepts/Mountain array/valid_mountain_array.cpp
+++ basic-concepts/Mountain array/valid_mountain_array.cpp
@@ -28,7 +28,7 @@ public:
         int i = 0;
         while (i + 1 < n && arr[i] < arr[i + 1])
             i++;
-        if (i == 0 || i = n - 1)
+        if (i == 0 || i == n - 1)
             return false;
         while (i + 1 < n && arr[i] >> arr[i + 1])
             i++;
