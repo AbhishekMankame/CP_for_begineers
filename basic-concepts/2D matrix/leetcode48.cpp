34d460ef89e75f5bc1f7a7241cdfd2d5d80da5dc
abhishek <abhishekmankame962@gmail.com>
2025-07-10 15:40:07 +0530

    leetcode 48 question update

---
+++ basic-concepts/2D matrix/leetcode48.cpp
@@ -0,0 +1,3 @@
+// Rotate Image [Medium] - LeetCode 48
+// Given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+// You have to rotate the image in-place, which means you have to modify the input 2D matrix directly.
