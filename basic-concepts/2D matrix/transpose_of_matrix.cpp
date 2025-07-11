bb2f0c67c52c6d07394b2a08f32b6e79cb4cc01e
abhishek <abhishekmankame962@gmail.com>
2025-07-11 23:52:17 +0530

    question and complexity update

--- basic-concepts/2D matrix/transpose_of_matrix.cpp
+++ basic-concepts/2D matrix/transpose_of_matrix.cpp
@@ -1,4 +1,8 @@
 // Transpose of a matrix - Given an n x n 2D matrix, compute the transpose of the matrix.
+/* Leetcode 867 - Given a 2D integer array `matrix`, return the transpose of `matrix`.
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+*/
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -17,3 +21,6 @@ vector<vector<int>> transpose(vector<vector<int>> &matrix)
     }
     return transpose;
 }
+
+// Time Complexity: O(n * m) - where n is the number of rows and m is the number of columns in the matrix.
+// Space Complexity: O(n * m) - for the transpose matrix.
