942e97e44bbbeeb30627abdeb1cc617cc45253e7
abhishek <abhishekmankame962@gmail.com>
2025-07-12 22:53:21 +0530

    leetcode 48 code update

--- basic-concepts/2D matrix/leetcode48.cpp
+++ basic-concepts/2D matrix/leetcode48.cpp
@@ -13,3 +13,26 @@
 -------------             -------------
 
 */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        int row = matrix.size();
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = i; j < row; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for (int i = 0; i < row; i++)
+        {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+}
