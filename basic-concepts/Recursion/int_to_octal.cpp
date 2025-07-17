68efb3f82ca37cf9e2aee1166007ef15f9925214
abhishek <abhishekmankame962@gmail.com>
2025-07-18 00:02:31 +0530

    int to octal code update

--- basic-concepts/Recursion/int_to_octal.cpp
+++ basic-concepts/Recursion/int_to_octal.cpp
@@ -1,1 +1,27 @@
 // Int to Octal Conversion using Recursion
+
+#include <iostream>
+using namespace std;
+
+void intToOctal(int n)
+{
+    if (n == 0)
+    {
+        cout << 0;
+        return;
+    }
+
+    int octal[100]; // to store octal bits
+    int i = 0;
+    while (n > 0)
+    {
+        octal[i] = n % 8;
+        n /= 8;
+        i++;
+    }
+
+    for (int j = i - 1; j >= 0; j--)
+    {
+        cout << octal[j];
+    }
+}
