fc1cf5101215eba4ed7f7fdc63a05167fcbe0b2d
abhishek <abhishekmankame962@gmail.com>
2025-07-18 00:32:15 +0530

    octal to decimal code update

---
+++ basic-concepts/Recursion/octal_to_int.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int octalToDecimal(int octal)
+{
+    int decimal = 0, base = 1;
+
+    while (octal > 0)
+    {
+        int lastDigit = octal % 10;
+        decimal += lastDigit * base;
+        base = base * 8;
+        octal = octal / 10;
+    }
+
+    return decimal;
+}
