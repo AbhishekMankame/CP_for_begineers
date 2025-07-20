8232d1f7b2cf5f9b08e860b9456b93ee1b147adb
abhishek <abhishekmankame962@gmail.com>
2025-07-20 23:59:58 +0530

    example

--- topics/recursion/intro.cpp
+++ topics/recursion/intro.cpp
@@ -1,1 +1,10 @@
 // Recursion: A function that calls itself to solve a problem
+// Example: Factorial of a number using recursion
+int factorial(int n)
+{
+    if (n <= 1)
+    {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
