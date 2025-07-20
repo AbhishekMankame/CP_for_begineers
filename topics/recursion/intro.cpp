333932d7359ea73278a7b5393fcf185ac1f4afc0
abhishek <abhishekmankame962@gmail.com>
2025-07-21 00:07:59 +0530

    factorial of a number using recursion code

--- topics/recursion/intro.cpp
+++ topics/recursion/intro.cpp
@@ -1,5 +1,9 @@
 // Recursion: A function that calls itself to solve a problem
 // Example: Factorial of a number using recursion
+
+#include <iostream>
+using namespace std;
+
 int factorial(int n)
 {
     if (n <= 1)
@@ -7,4 +11,12 @@ int factorial(int n)
         return 1;
     }
     return n * factorial(n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << "Factorial of " << n << " is: " << factorial(n) << endl;
+    return 0;
 }
