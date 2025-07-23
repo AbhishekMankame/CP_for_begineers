9479d528e76291a306a85f74ae51b5e6d006cc07
abhishek <abhishekmankame962@gmail.com>
2025-07-23 22:28:28 +0530

    fibonacci optimized approach

---
+++ topics/recursion/fibonacci_optimized_approach.cpp
@@ -0,0 +1,3 @@
+// Fibonacci Optimized Approach in C++
+// This program calculates the nth Fibonacci number using an optimized approach.
+// The Fibonacci sequence is defined as: F(0) = 0, F(1) = 1, F(n) = F(n - 1) + F(n - 2) for n > 1.
