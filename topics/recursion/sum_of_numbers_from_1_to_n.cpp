3b6d8c08ab7dec2eae293b1e40ee6456ea66e680
abhishek <abhishekmankame962@gmail.com>
2025-07-27 23:47:08 +0530

    details update additional

--- topics/recursion/sum_of_numbers_from_1_to_n.cpp
+++ topics/recursion/sum_of_numbers_from_1_to_n.cpp
@@ -22,3 +22,4 @@ int main()
 // Time Complexity: O(n) --> The time complexity is O(n) because the function makes a single recursive call for each number from n down to 1, leading to n calls in total.
 // Space Complexity: O(n) --> The space complexity is O(n) due to the call stack used for recursion, which can go up to n levels deep in the worst case.
 // Note: This implementation is straightforward and effective demonstrates the use of recursion to calculate the sum of numbers. It is efficient for moderate values of n, but for very large n, consider using an iterative approach to avoid deep recursion issues.
+// Deep Recursion: If n is very large, this implementation may lead to a stack overflow due to deep recursion. In such cases, an iterative approach or tail recursion optimization (if supported by the compiler) would be advisable.
