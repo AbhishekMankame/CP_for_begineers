5368c0501907fde3fe98a9e4880c74a8ab7cbce1
abhishek <abhishekmankame962@gmail.com>
2025-07-28 01:46:57 +0530

    complexity update

--- topics/recursion/digits_of_number.cpp
+++ topics/recursion/digits_of_number.cpp
@@ -11,3 +11,7 @@ int main()
     cout << digits << endl;
     return 0;
 }
+
+// Time Complexity: O(1) --> The time complexity is O(1) because the logarithmic operation log10(n) is computed in constant time, regardless of the size of n.
+// Space Complexity: O(1) --> The space complexity is O(1) as we are using a fixed amount of space for the variable 'digits' and do not use any additional data structures that grow with input size.
+// Note: This implementation efficiently calculates the number of digits in a given integer n using the logarithmic function. It is suitable for all positive integers, including very large numbers as long as they fit within the limits of the data type used (int in this case).
