e3c3eb6a07d68c7bbd780b4584b6af7c049eba7e
abhishek <abhishekmankame962@gmail.com>
2025-07-29 23:49:49 +0530

    complexity details update

--- topics/recursion/check_if_array_sorted_or_not.cpp
+++ topics/recursion/check_if_array_sorted_or_not.cpp
@@ -15,6 +15,12 @@ bool isSorted(vector<int> arr, int n)
 int main()
 {
     vector<int> arr = {1, 2, 3, 4, 5};
-
+    int n = arr.size();
+    cout << isSorted(arr, n) << endl;
     return 0;
 }
+
+// Time Complexity: O(n) --> The time complexity is O(n) because we are checking each element of the array recursively, leading to n recursive calls.
+// Space Complexity: O(n) --> The space complexity is O(n) due to the recursive call stack, which can go as deep as n levels in the worst case.
+// Note: This implementation checks if the array is sorted in non-decreasing order. It compares each element with the previous one and recursively checks the rest of the array.
+// How this code works: The function `isSorted` checks if the last element is greater than or equal to the second last element and then recursively calls itself with the rest of the array. If it reaches the base case where the size of the array is 0 or 1, it return true, indicating that the array is sorted.
