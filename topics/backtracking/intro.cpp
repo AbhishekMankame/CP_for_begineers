ca34f0993fb6fa698ee47488403c0773ece9654e
abhishek <abhishekmankame962@gmail.com>
2025-07-29 10:49:14 +0530

    backtracking introduction

---
+++ topics/backtracking/intro.cpp
@@ -0,0 +1,3 @@
+// Backtracking Introduction
+// Backtracking is a technique used to build all possible solutions to a problem incrementlly and abandon (or backtrack) when we realize we can't reach a valid solution.
+// Eg: It's like exploring a maze: if you hit a dead end, you go back and try another path.
