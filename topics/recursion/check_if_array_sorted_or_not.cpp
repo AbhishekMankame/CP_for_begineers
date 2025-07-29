5077bd8356242f9d2a343f734f6306459060beb2
abhishek <abhishekmankame962@gmail.com>
2025-07-29 23:07:14 +0530

    checking if given array is sorted or not

---
+++ topics/recursion/check_if_array_sorted_or_not.cpp
@@ -0,0 +1,20 @@
+// To check if the given array is sorted or not
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// check if array is sorted
+bool isSorted(vector<int> arr, int n)
+{
+    if (n == 0 || n == 1)
+        return true;
+
+    return arr[n - 1] >= arr[n - 2] && isSorted(arr, n - 1);
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+
+    return 0;
+}
