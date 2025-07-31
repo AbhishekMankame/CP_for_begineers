ca2a586493298d8ebd6e64235af064848c705479
abhishek <abhishekmankame962@gmail.com>
2025-07-31 23:39:15 +0530

    backtracking print all subsets

---
+++ topics/backtracking/print_all_subsets.cpp
@@ -0,0 +1,14 @@
+// Printing all subsets of a set using backtracking
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printSubsets(vector<int> &arr, vector<int> &res, int i)
+{
+}
+
+int main()
+{
+
+    return 0;
+}
