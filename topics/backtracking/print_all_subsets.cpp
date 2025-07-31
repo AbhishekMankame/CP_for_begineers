58b6781bf7b9d9ecb6dc36338ecf69787b6aacfb
abhishek <abhishekmankame962@gmail.com>
2025-07-31 23:42:45 +0530

    base condition update

--- topics/backtracking/print_all_subsets.cpp
+++ topics/backtracking/print_all_subsets.cpp
@@ -3,12 +3,24 @@
 #include <vector>
 using namespace std;
 
-void printSubsets(vector<int> &arr, vector<int> &res, int i)
+void printSubsets(vector<int> &arr, vector<int> &res, int i) // Here we pass ans by reference as we don't want a copy (as we want to make changes in the same subset)
 {
+    if (i == arr.size())
+    {
+        for (int val : arr)
+        {
+            cout << val << " ";
+        }
+        cout << endl;
+        return;
+    }
 }
 
 int main()
 {
+    vector<int> arr = {1, 2, 3};
+    vector<int> ans; // store subsets
 
+    printSubsets(arr, ans, 0); // here 0 is index from where we will start
     return 0;
 }
