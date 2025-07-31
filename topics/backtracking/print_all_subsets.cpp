f99647aa1ab374fb770fc85d0c41f3b0e95a2d5c
abhishek <abhishekmankame962@gmail.com>
2025-08-01 00:13:05 +0530

    leetcode 78 question update

--- topics/backtracking/print_all_subsets.cpp
+++ topics/backtracking/print_all_subsets.cpp
@@ -32,3 +32,7 @@ int main()
     printSubsets(arr, ans, 0); // here 0 is index from where we will start
     return 0;
 }
+
+// Time Compelxity: O(2^n) --> The time complexity is O(2^n) because for each element in the array, we have two choices: to include it in the subset or not. This leads to a total of 2^n possible subsets for an array of size n.
+// Space Complexity: O(n) --> The space complexity is O(n) due to the recursive call stack, which can go as deep as n levels in the worst case, where n is the size of the input array. Additionally, the space used for storing subsets can also be O(n) in the worst case when all elements are included in a subset.
+// Note: This implementation uses backtracking to print all subsets of a given set. It explores both possibilities for each element: including it in the current subset or excluding it, and recursively builds all possible subsets.
