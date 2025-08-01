fa41c2cf1edd5addb8f2f81aed4e0e786f3dbbe9
abhishek <abhishekmankame962@gmail.com>
2025-08-01 12:20:52 +0530

    code and compelxity update

--- topics/backtracking/subsets-2.cpp
+++ topics/backtracking/subsets-2.cpp
@@ -11,3 +11,48 @@ Example 2:
 Input: nums = [0]
 Output: [[],[0]]
 */
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    void getSubset(vector<int> &nums, vector<int> &ans, int i, vector<vector<int>> &allSubset)
+    {
+        if (i == nums.size())
+        {
+            allSubset.push_back(ans);
+            return;
+        }
+
+        // Inclusion code
+        ans.push_back(nums[i]);
+        getSubset(nums, ans, i + 1, allSubset);
+
+        // Backtrack step
+        ans.pop_back();
+
+        // Exclusion code
+        int index = i + 1;
+        while (index < nums.size() && nums[i] == nums[i - 1])
+        {
+            index++;
+        }
+        getSubset(nums, ans, index, allSubset);
+    }
+
+    vector<vector<int>> subsetsWithDup(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> allSubset;
+        vector<int> ans;
+        getSubset(nums, ans, 0, allSubset);
+        return allSubset;
+    }
+};
+
+// Time Complexity: O(n * (2^n))
+// Space Compelxity: O(n)
+// Note: The solution uses sorting to handle duplicates and enusre that the subsets are unique.
