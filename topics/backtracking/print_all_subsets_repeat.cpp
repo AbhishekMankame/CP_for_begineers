4540694b21844c905f2876f56e3c1884f752b5e3
abhishek <abhishekmankame962@gmail.com>
2025-08-03 23:20:57 +0530

    repeating the question update

---
+++ topics/backtracking/print_all_subsets_repeat.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void getSubset(vector<int> &arr, vector<int> &ans, int i)
+{
+    if (i == arr.size())
+    {
+        for (auto val : ans)
+        {
+            cout << val << " ";
+        }
+        cout << endl;
+        return;
+    }
+
+    // Exclusion
+    getSubset(arr, ans, i + 1);
+    // Inclusion
+    ans.push_back(arr[i]);
+    getSubset(arr, ans, i + 1);
+    // Backtrack case
+    ans.pop_back();
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3};
+    vector<int> ans; // store subsets
+
+    getSubset(arr, ans, 0); // here 0 is index from where we will start
+    return 0;
+}
