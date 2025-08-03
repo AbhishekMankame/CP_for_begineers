22b3898db95c2bc78f4f9a171cc4f249472d48e0
abhishek <abhishekmankame962@gmail.com>
2025-08-03 23:03:19 +0530

    Print all subset lc waala question solving for second time

---
+++ topics/backtracking/subsets 2_repeat.cpp
@@ -0,0 +1,34 @@
+// This is the same code as for Subsets 2, just for practice
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    void getSubset(vector<int> &arr, vector<int> &ans, int i, vector<vector<int>> &allSubset)
+    {
+        if (i == arr.size())
+        {
+            allSubset.push_back(ans);
+            return;
+        }
+
+        // Inclusion
+        ans.push_back(arr[i]);
+        getSubset(arr, ans, i + 1, allSubset);
+
+        // Backtrack waala step
+        ans.pop_back();
+
+        // Exclusion
+        int index = i + 1;
+        while (index < arr.size() && arr[i] == arr[i - 1])
+        {
+            index++;
+        }
+        getSubset(arr, ans, index, allSubset);
+    }
+};
