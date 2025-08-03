22e764d19978777342e04f63aee3f6915bc310e3
abhishek <abhishekmankame962@gmail.com>
2025-08-04 00:23:56 +0530

    code for permutation using recursion update

---
+++ topics/backtracking/permutations.cpp
@@ -0,0 +1,33 @@
+// Permutation
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    void getPerms(vector<int> &nums, int idx, vector<vector<int>> &ans)
+    {
+        if (idx == nums.size())
+        {
+            ans.push_back({nums});
+            return;
+        }
+
+        for (int i = idx; i < nums.size(); i++)
+        {
+            swap(nums[idx], nums[i]); // idx place => ith element choice
+            getPerms(nums, i + 1, ans);
+            swap(nums[idx], nums[i]); // backtrack
+        }
+    }
+
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        vector<vector<int>> ans;
+        getPerms(nums, 0, ans);
+        return ans;
+    }
+};
