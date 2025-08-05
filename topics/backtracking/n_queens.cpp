32c4c2ae32ed93c1f3d027e13f98a4246ae11a6f
abhishek <abhishekmankame962@gmail.com>
2025-08-05 10:34:32 +0530

    nQueens update

--- topics/backtracking/n_queens.cpp
+++ topics/backtracking/n_queens.cpp
@@ -8,7 +8,28 @@ using namespace std;
 class Solution
 {
 public:
+    void nQueens(vector<string> &board, int row, int n, vector<vector<string>> &ans)
+    {
+        if (row == n)
+        {
+            ans.push_back({board});
+            return;
+        }
+
+        for (int j = 0; j < board.size(); j++)
+        {
+            if (isSafe(board, row, j, n))
+            {
+                board[row][j] = 'Q';
+                nQueens(board, row + 1, n, ans);
+                board[row][j] = '.';
+            }
+        }
+    }
+
     vector<vector<string>> solveNQueens(int n)
     {
+        vector<string> board;
+        vector<vector<string>> ans;
     }
 };
