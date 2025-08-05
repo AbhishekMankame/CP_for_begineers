f7efab8cc9134dd8eeba2972a81e6c29d28ad676
abhishek <abhishekmankame962@gmail.com>
2025-08-05 08:44:15 +0530

    nQueens question

---
+++ topics/backtracking/n_queens.cpp
@@ -0,0 +1,14 @@
+// N-Queens problem using backtracking
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<string>> solveNQueens(int n)
+    {
+    }
+};
