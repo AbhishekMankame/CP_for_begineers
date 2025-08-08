937bfcee7a7e3f26c3801b18877992ac8fe69367
abhishek <abhishekmankame962@gmail.com>
2025-08-08 15:34:47 +0530

    constants update

---
+++ Project/path_finder_astro/newton's_laws.cpp
@@ -0,0 +1,15 @@
+// C++ Code: Newton's Law of Gravitation
+// Concepts used: F = G * (m1 * m2) / r^2, F = ma, Euler integration
+
+#include <iostream>
+#include <cmath>
+#include <fstream> // For file output
+using namespace std;
+
+// Constants
+const double G = 6.67430e-11;    // gravitational constant (m^3 kg^-1 s^-2)
+const double M_sun = 1.989e30;   // mass of the sun (kg)
+const double M_earth = 5.972e24; // mass of the earth (kg)
+const double AU = 1.496e11;      // 1 astronomical unit in meters (distance from Earth to Sun)
+const double dt = 60 * 60;       // 1 hour in seconds
+const int steps = 8760;
