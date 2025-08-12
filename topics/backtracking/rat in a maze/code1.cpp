f13cdffb10327d760ffce01dc829556777ba3ef9
abhishek <abhishekmankame962@gmail.com>
2025-08-12 08:33:27 +0530

    rat in a maze code update

--- topics/backtracking/rat in a maze/code1.cpp
+++ topics/backtracking/rat in a maze/code1.cpp
@@ -3,10 +3,10 @@
 #include <vector>
 using namespace std;
 
-void helper(vector<vector<int>> &mat, int r, int c, string path, vector<string> &ans)
+void helper(vector<vector<int>> &mat, int r, int c, string path, vector<string> &ans, vector<vector<bool>> &vis)
 {
     int n = mat.size();
-    if (r < 0 || c < 0 || r >= n || c >= n || mat[r][c] == 0)
+    if (r < 0 || c < 0 || r >= n || c >= n || mat[r][c] == 0 || vis[r][c] == true)
     {
         return;
     }
@@ -18,17 +18,25 @@ void helper(vector<vector<int>> &mat, int r, int c, string path, vector<string> 
         return;
     }
 
-    helper(mat, r + 1, c, path + "D", ans); // down
-    helper(mat, r - 1, c, path + "D", ans); // up
-    helper(mat, r, c + 1, path + "R", ans); // right
-    helper(mat, r, c - 1, path + "L", ans); // left
+    vis[r][c] = true; // yaha par isliye place kiya hai ki, jab next waale saare calls lagao toh unko pata rahe ki that particular (r,c) is visited cell
+
+    helper(mat, r + 1, c, path + "D", ans, vis); // down
+    helper(mat, r - 1, c, path + "D", ans, vis); // up
+    helper(mat, r, c + 1, path + "R", ans, vis); // right
+    helper(mat, r, c - 1, path + "L", ans, vis); // left
+
+    // Most IMP: Backtracking step in the same problem is ki, ek baar humne visited ko true maan liya, aur humne calls lagayi, toh hum jab inn calls se waapis aayenge, toh dobara, hume visited ko false mark karna padega
+
+    vis[r][c] = false;
 }
 
 vector<string> findPath(vector<vector<int>> &mat)
 {
+    int n = mat.size();
     vector<string> ans;
     string path = "";
-    helper(mat, 0, 0, path, ans);
+    vector<vector<bool>> vis(n, vector<bool>(n, false));
+    helper(mat, 0, 0, path, ans, vis);
 
     return ans;
 }
