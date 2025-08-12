7030573a75ae02bb90d18e3d93fd903518037505
abhishek <abhishekmankame962@gmail.com>
2025-08-12 08:21:03 +0530

    base condition update

--- topics/backtracking/rat in a maze/code1.cpp
+++ topics/backtracking/rat in a maze/code1.cpp
@@ -5,6 +5,19 @@ using namespace std;
 
 void helper(vector<vector<int>> &mat, int r, int c, string path, vector<string> &ans)
 {
+    int n = mat.size();
+    if (r < 0 || c < 0 || r >= n || c >= n || mat[r][c] == 0)
+    {
+        return;
+    }
+
+    // answer waala base case
+    if (r == n - 1 && c == n - 1)
+    {
+        ans.push_back(path);
+        return;
+    }
+
     helper(mat, r + 1, c, path + "D", ans); // down
     helper(mat, r - 1, c, path + "D", ans); // up
     helper(mat, r, c + 1, path + "R", ans); // right
