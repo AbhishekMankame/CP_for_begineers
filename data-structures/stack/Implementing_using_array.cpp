6581d46634436597872658f75eaf7f42e16618e7
abhishek <abhishekmankame962@gmail.com>
2025-08-15 08:00:46 +0530

    complete code update

--- data-structures/stack/Implementing_using_array.cpp
+++ data-structures/stack/Implementing_using_array.cpp
@@ -47,3 +47,16 @@ public:
         return topIndex == -1;
     }
 };
+
+int main()
+{
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    cout << "Top: " << s.top() << endl;
+    s.pop();
+    cout << "Top after pop: " << s.top() << endl;
+    return 0;
+}
