f639ac0d7f7b6d5703232791ae81b9db50cfd4bc
abhishek <abhishekmankame962@gmail.com>
2025-08-17 12:47:42 +0530

    longest common prefix function update

---
+++ leetcode/14_longest_common_prefix.cpp
@@ -0,0 +1,46 @@
+// Leetcode 14. Longest Common Prefix
+
+/*Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string ""
+
+Example 1:
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+
+Constraints:
+1 <= strs. length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters if it is non-empty
+
+Topics: Array String Trie
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string longestCommonPrefix(vector<string> &str)
+{
+
+    if (str.empty())
+        return "";
+
+    sort(str.begin(), str.end());
+    int n = str.size();
+    string ans = "";
+
+    string st = str[0], end = str[n - 1];
+
+    for (int i = 0; i < min(st.size(), end.size()); i++)
+    {
+        if (st[i] != end[i])
+        {
+            break;
+        }
+        ans = ans + st[i];
+    }
+    return ans;
+}
