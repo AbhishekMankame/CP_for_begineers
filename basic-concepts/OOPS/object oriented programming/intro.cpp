fc68279122d08753233e120f7048c05b6c8f96af
abhishek <abhishekmankame962@gmail.com>
2025-08-22 11:42:37 +0530

    parameterized and non parameterized constructors

--- basic-concepts/OOPS/object oriented programming/intro.cpp
+++ basic-concepts/OOPS/object oriented programming/intro.cpp
@@ -9,9 +9,20 @@ private:
     double salary; // Here we have made salary as private and other things as public
 
 public:
-    Teacher()
+    // // Non-parameterized
+    // Teacher()
+    // {
+    //     // cout << "Hi, I am constructor\n";
+    //     dept = "Computer Science";
+    // }
+
+    // Parameterized
+    Teacher(string n, string d, string s, double sal)
     {
-        cout << "Hi, I am constructor\n";
+        name = n;
+        dept = d;
+        subject = s;
+        salary = sal;
     }
     // Properties
     string name;
@@ -35,6 +46,14 @@ public:
     {
         return salary;
     }
+
+    void getInfo()
+    {
+        cout << "name: " << name << endl;
+        cout << "subject: " << subject << endl;
+        cout << "Department: " << dept << endl;
+        cout << "Salary: " << salary << endl;
+    }
     // salary ko hum directly access nahi kar paa rahe, par indirectly inn public functions ke through access kar paa rahe hai
 }; // Here semicolon is necessary, it will denote the end of the class
 
@@ -48,14 +67,18 @@ public:
 
 int main()
 {
-    Teacher t1; // Yaha par internally constructor ko call laga rahi hai, compiler automatically constructor ko kar deta hai
-    Teacher t2;
-    t1.name = "Andrew Ng";
-    t1.subject = "Machine Learning";
-    t1.dept = "CS";
-    t1.setSalary(25000);
+    // Teacher t1; // Yaha par internally constructor ko call laga rahi hai, compiler automatically constructor ko kar deta hai
+    // Teacher t2;
+    // t1.name = "Andrew Ng";
+    // t1.subject = "Machine Learning";
+    // // t1.dept = "CS";
+    // t1.setSalary(25000);
+
+    Teacher t1("Shradha", "Computer Science", "C++", 25000);
 
     cout << t1.name << endl;
     cout << t1.getSalary() << endl;
+    cout << t1.dept << endl;
+    t1.getInfo();
     return 0;
 }
