ec8fa887cfe993040c98f52b28cf18b3b0defadd
abhishek <abhishekmankame962@gmail.com>
2025-08-25 11:08:43 +0530

    deep copy

--- basic-concepts/OOPS/object oriented programming/deep_copy.cpp
+++ basic-concepts/OOPS/object oriented programming/deep_copy.cpp
@@ -14,4 +14,26 @@ public:
         cgpaPtr = new double;
         *cgpaPtr = cgpa;
     }
+
+    Student(Student &obj)
+    {
+        this->name = obj.name;
+        this->cgpaPtr = obj.cgpaPtr;
+    }
+
+    void getInfo()
+    {
+        cout << "Name: " << name << endl;
+        cout << "CGPA: " << *cgpaPtr << endl;
+    }
 };
+
+int main()
+{
+    Student s1("Rahul Kumar", 8.9);
+    Student s2(s1);
+    s1.getInfo();
+    *(s2.cgpaPtr) = 9.2;
+    s2.getInfo();
+    return 0;
+}
