0271bd0001fa6aa7692e578d7b8e2dee7b81af95
abhishek <abhishekmankame962@gmail.com>
2025-08-25 11:15:24 +0530

    shallow copy vs deep copy

--- basic-concepts/OOPS/object oriented programming/deep_copy.cpp
+++ basic-concepts/OOPS/object oriented programming/deep_copy.cpp
@@ -15,10 +15,19 @@ public:
         *cgpaPtr = cgpa;
     }
 
+    // Shallow copy
+    // Student(Student &obj)
+    // {
+    //     this->name = obj.name;
+    //     this->cgpaPtr = obj.cgpaPtr;
+    // }
+
+    // Deep Copy
     Student(Student &obj)
     {
         this->name = obj.name;
-        this->cgpaPtr = obj.cgpaPtr;
+        cgpaPtr = new double;
+        *cgpaPtr = *obj.cgpaPtr;
     }
 
     void getInfo()
@@ -33,6 +42,7 @@ int main()
     Student s1("Rahul Kumar", 8.9);
     Student s2(s1);
     s1.getInfo();
+    s2.name = "Neha";
     *(s2.cgpaPtr) = 9.2;
     s2.getInfo();
     s1.getInfo();
