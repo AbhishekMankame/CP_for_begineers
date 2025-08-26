3f5b15b7ce9b88bd99ec3d7dc327799e5d00c1c0
abhishek <abhishekmankame962@gmail.com>
2025-08-26 23:06:06 +0530

    strobogrammatic

---
+++ OOPS/OOPS/object oriented programming/abstract_class.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Shape
+{
+    virtual void draw() = 0; // Pure virtual function
+};
---
+++ concepts/strobogrammatic number/introduction.cpp
@@ -0,0 +1,19 @@
+/*What is a Strobogrammatic Number?
+A number is strobogrammatic if it looks the same when rotated 180 degrees.
+For example:
+- Valid: 69, 96, 00, 88
+- Invalid: 23, 962, 110
+
+The valid digit pairs are:
+- 0 <-> 0
+- 1 <-> 1
+- 6 <-> 9
+- 8 <-> 8
+- 9 <-> 6
+
+Variations of Strobogrammatic Problems
+1. Check if a number is strobogrammatic
+2. Generate all strobogrammatic numbers of length `n`
+3. Count strobogrammtic numbers within a range `[low, high]`
+
+*/
