eb1376d1abc30a2d87bebd1d14398f25a6e02701
abhishek <abhishekmankame962@gmail.com>
2025-08-26 11:38:01 +0530

    leetcode 1886 question update

---
+++ leetcode/1886_Determine_whether_matrix_can_be_obtained_by_rotation.cpp
@@ -0,0 +1,19 @@
+// Leetcode 1886 - Determine Whether Matrix Can Be Obtained By Rotation  --> Easy
+/*Given two `n*n` binary matrices `mat` and `target`, return `true` if it is possible to make `mat` equal to `target` by rotating `mat` in 90-degree increments, or `false` otherwise.
+
+
+Example 1:
+
+
+Input: mat = [[0,1],[1,0]], target = [[1,0],[0,1]]
+Output: true
+Explanation: We can rotate mat 90 degrees clockwise to make mat equal target.
+
+
+Constraints:
+n == mat.length == target.length
+n == mat[i].length == target[i].length
+1 <= n <= 10
+mat[i][j] and target[i][j] are either 0 or 1.
+
+*/
