2ba9543164a42244c834c57097855443e8d07a35
abhishek <abhishekmankame962@gmail.com>
2025-08-27 15:52:48 +0530

    abstract class

--- OOPS/OOPS/object oriented programming/abstract_class.cpp
+++ OOPS/OOPS/object oriented programming/abstract_class.cpp
@@ -6,3 +6,19 @@ class Shape // Jis bhi class ke andar pure virtual function hota hai, woh automa
 {
     virtual void draw() = 0; // Pure virtual function
 };
+
+class Circle : public Shape
+{
+public:
+    void draw()
+    {
+        cout << "Drawing a circle\n";
+    }
+};
+
+int main()
+{
+    Circle c1;
+    c1.draw();
+    return 0;
+}
