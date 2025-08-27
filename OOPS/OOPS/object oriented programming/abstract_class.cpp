3acfaabb6c32f50fedc1c4ed8d155d4ac4fda0c1
abhishek <abhishekmankame962@gmail.com>
2025-08-27 15:45:51 +0530

    abstract class

--- OOPS/OOPS/object oriented programming/abstract_class.cpp
+++ OOPS/OOPS/object oriented programming/abstract_class.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-class Shape
+class Shape // Jis bhi class ke andar pure virtual function hota hai, woh automatically abstract class ban jaati hai
 {
     virtual void draw() = 0; // Pure virtual function
 };
