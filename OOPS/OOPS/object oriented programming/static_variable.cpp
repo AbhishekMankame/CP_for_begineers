f7e2fc867b9304b185ad572d199e753c715880fd
abhishek <abhishekmankame962@gmail.com>
2025-08-27 16:09:38 +0530

    simple update

--- OOPS/OOPS/object oriented programming/static_variable.cpp
+++ OOPS/OOPS/object oriented programming/static_variable.cpp
@@ -2,6 +2,7 @@
 #include <string>
 using namespace std;
 
+// Static variable in function
 void fun()
 {
     static int x = 0; // Initialize statement 1 hi baar run karegi
@@ -9,10 +10,25 @@ void fun()
     x++;
 }
 
+// Static variable in class
+class A
+{
+public:
+    int x;
+    void incX()
+    {
+        x = x + 1;
+    }
+};
+
 int main()
 {
-    fun();
-    fun();
-    fun();
+    A obj1;
+    A obj2;
+    obj1.x = 100;
+    obj2.x = 200;
+    cout << obj1.x << endl;
+    // obj.incX();
+    cout << obj2.x << endl;
     return 0;
 }
