32c0634be3f5415a0782c1c84dbc63532e3766c9
abhishek <abhishekmankame962@gmail.com>
2025-08-28 22:36:53 +0530

    linked list intro

--- Linked List/intro.cpp
+++ Linked List/intro.cpp
@@ -2,3 +2,37 @@
 - Some data
 - A pointer to the next node
 */
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
