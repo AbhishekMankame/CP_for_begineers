987f207487b5b41a950a2f2ccf9e19d519dc815f
abhishek <abhishekmankame962@gmail.com>
2025-08-30 10:27:57 +0530

    binary search intro

---
+++ STL/Binary Search/intro.cpp
@@ -0,0 +1,7 @@
+/*
+Binary Search --> It is a searching algorithm that finds the position of any element in a sorted array in logarithmic time.
+- It works by repeatedly dividing the search interval in half.
+- If the value of the search key is less than the item in the middle of the interval, narrow the interval to the lower half.
+- Otherwise, narrow it to the upper half.
+- Repeatedly check until the value is found or the interval is empty.
+*/
