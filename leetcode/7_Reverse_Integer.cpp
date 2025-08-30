b7bcc92de9f407c0a5b2dad7046f11f2a3650d44
abhishek <abhishekmankame962@gmail.com>
2025-08-30 09:06:53 +0530

    code update

--- leetcode/7_Reverse_Integer.cpp
+++ leetcode/7_Reverse_Integer.cpp
@@ -22,3 +22,24 @@ Constraints:
 
 Topics: Math
 */
+
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    int reverse(int x)
+    {
+        long long rev = 0;
+        while (x)
+        {
+            int last = x % 10;
+            rev = rev * 10 + last;
+            x = x / 10;
+        }
+        if (rev < INT_MIN || rev > INT_MAX)
+            return 0;
+        return (int)rev;
+    }
+};
