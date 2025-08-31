b34035aa374701e4b7f024459263005f3902eb1b
abhishek <abhishekmankame962@gmail.com>
2025-08-31 12:02:42 +0530

    graph introduction

---
+++ Graphs/introduction.cpp
@@ -0,0 +1,9 @@
+// Graphs: Introduction
+/*Graph is a collection of nodes and edges. It is a data structure that represents relationships between objects. Graphs can be directed or undirected, weighted or unweighted. They are used in various applications such as social networks, recommendation systems, and transportation networks.*/
+
+/* Storing a Graph
+- Adjacency List
+- Adjacency Matrix
+- Edge List
+- 2D Matrix (Implicit Graph)
+*/
