f0cd7e73fb984d79be0ea054fd00c88c3d402178
abhishek <abhishekmankame962@gmail.com>
2025-08-31 12:22:51 +0530

    types of graphs

--- Graphs/introduction.cpp
+++ Graphs/introduction.cpp
@@ -12,3 +12,18 @@
 - Too much memory is required
 - Just to check the neighbours we need O(v) time to check entire row
 */
+
+/*Types of Graph
+- Weighted Graph
+- Unweighted Graph
+- Undirected Graph
+- Directed Graph
+*/
+
+// Weighted Graph --> Graphs whose edges or paths have values called weights. Edges value can be represent weight/cost/length
+
+// Unweighted Graph --> There is no value or weight associated with the edge. By default, all the graphs are unweighted unless there is a value associated.
+
+// Directed Graph --> Edges are directed from one node to another. For example, Flights Routes, Flight Fares
+
+// Undirected Graph --> All edges are bi-directional. For example, Friends connected on facebook
