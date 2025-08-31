7e3111859f11da736becee30a6b4b273400a1fd3
abhishek <abhishekmankame962@gmail.com>
2025-08-31 23:30:48 +0530

    brute force code update

--- leetcode/4_median_of_two_sorted_arrays.cpp
+++ leetcode/4_median_of_two_sorted_arrays.cpp
@@ -25,3 +25,24 @@ nums2.length == n
 -106 <= nums1[i], nums2[i] <= 106
 
 */
+
+// Brute force approach
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    double findMedianSortedArray(vector<int> &nums1, vector<int> &nums2)
+    {
+        nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+        sort(nums1.begin(), nums1.end());
+        int n = nums1.size();
+        if (n % 2 == 1)
+            return nums1[n / 2];
+        else
+            return (nums1[n / 2 - 1] + nums2[n / 2]) / 2.0;
+    }
+};
