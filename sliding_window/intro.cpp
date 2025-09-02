67eec9d8ab5e2a0cf7fe95f69cfc00bd0429b04d
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-02 17:18:28 +0000

    code update

--- sliding_window/intro.cpp
+++ sliding_window/intro.cpp
@@ -11,3 +11,34 @@ Subarray of size 3 are:
 [5, 1, 3] -> sum = 9 <- max sum
 [1, 3, 2] -> sum = 6
 */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int maxSumSubarray(vector<int> &arr, int k){
+    int n = arr.size();
+    int window_sum=0;
+    int max_sum=0;
+
+    // sum of first `k` elements
+    for(int i=0;i<k;i++){
+        window_sum += arr[i];
+    }
+    max_sum = window_sum;
+
+    // Slide the window
+    for(int i=k;i<n;i++){
+        window_sum += arr[i] - arr[i-k]; // Add next element, remove first element of previous window
+        max_sum = max(max_sum, window_sum);
+    }
+
+    return max_sum;
+}
+
+int main() {
+    vector<int> arr = {2, 1, 5, 1, 3, 2};
+    int k=3;
+    cout << "Maximum sum of subarray of size "<< k << " is "<<maxSumSubarray(arr, k) << endl;
+    return 0;
+}
