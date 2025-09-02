064faeeac622a225c10da74e6ab428ba807e03c5
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-02 17:12:03 +0000

    maximum sum of subarray of size k question update

---
+++ sliding_window/intro.cpp
@@ -0,0 +1,13 @@
+// Problem: Find maximum sum of any subarray of size k
+/*Given an array of integers arr[] and a number k, find the maximum sum of any contiguous subarray of size k.
+
+Input: arr = [2, 1, 5, 1, 3, 2], k=3
+Output: 9
+
+Explanation:
+Subarray of size 3 are:
+[2, 1, 5] -> sum = 8
+[1, 5, 1] -> sum = 7
+[5, 1, 3] -> sum = 9 <- max sum
+[1, 3, 2] -> sum = 6
+*/
