e62cecf3cd6647b8208513061bd048bd5f7ff302
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-06 12:40:10 +0000

    ternary search introduction

---
+++ Ternary_search/intro.cpp
@@ -0,0 +1,6 @@
+// Ternary Search
+/*
+Ternary search is a divide and conquer algorithm similar to binary search, but instead of dividing the array into two parts, it divides the array into three parts.
+
+It is typically used for finding the maximum and minimum of a unimodal function, or searching in a sorted array (though binary search is more efficient for arrays).
+*/
