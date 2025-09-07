c9fc827ad87cf7dc17ce5d549ac01f042da5644f
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-07 11:07:02 +0000

    typdef update

--- STL-CP/intro.cpp
+++ STL-CP/intro.cpp
@@ -1,11 +1,27 @@
 #include<iostream>
 #include<bits/stdc++.h>
-using namespace st d;
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef pair<int, int> pii;
+typedef pair<long, log> pll;
+typedef vector<pii> vpii;
+
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int n=5;
     vector<int>v; // size is not defined
     vector<int>v1(n);
     vector<int>v2(n,0);
+
+    // push_back() --> Adds element to the end of the vector
+    v.push_back(1);
+    v.size();
+    v.capacity();
     return 0;
 }
