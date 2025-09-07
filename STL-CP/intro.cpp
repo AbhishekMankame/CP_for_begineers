840cdb13471be9a407f88703b3348d54c9459510
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-07 04:38:53 +0000

    dtl vector intro

---
+++ STL-CP/intro.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace st d;
+
+int main() {
+    int n=5;
+    vector<int>v; // size is not defined
+    vector<int>v1(n);
+    vector<int>v2(n,0);
+    return 0;
+}
