b672cf2a0aa37dcf71206508399efb2121519130
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-07 16:25:53 +0000

    convex hull intro

---
+++ Mathematics/Convex Hull/Andrew_monotonic_chain.cpp
@@ -0,0 +1,2 @@
+// Convex Hull: It is a classic computational geometry problem:
+// Given a set of points in the 2D plane, find the smallest convex polygon (convex hull) that contains all the points.
