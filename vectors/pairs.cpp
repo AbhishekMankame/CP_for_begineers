3a44ee9d0706be54217e74bc14394b5e35e5a8b0
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-08 05:20:13 +0000

    pairs question update

---
+++ vectors/pairs.cpp
@@ -0,0 +1,11 @@
+// Pairs
+/*Given an array containing N integers, and an number S denoting a target sum. 
+
+Find two distinct integers that can pair up to form target sum. Let us assume there will be only one suct pair.
+
+Input:
+array = [10, 5, 2, 3, -6, 9, 11]
+S = 4
+
+Output: [10, -6]
+*/
