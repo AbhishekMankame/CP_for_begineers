6fb036232dc470cd703a8e72275b75a2e4b820f9
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-08 05:30:10 +0000

    code template update

--- vectors/pairs.cpp
+++ vectors/pairs.cpp
@@ -9,3 +9,23 @@ S = 4
 
 Output: [10, -6]
 */
+
+// Solving using hashmap approach
+
+#include<iostream>
+using namespace std;
+
+vector<int> pairSum(vector<int> arr, int ){
+
+    
+}
+
+int main() {
+    vector<int> arr{10, 5, 2, 3, -6, 9, 11};
+    int S = 4;
+    auto p = pairSum(arr,S);
+    if(p.size()==0) cout << "No such pair";
+    else cout <<p[0]<<","<<p[1]<<endl;
+
+    return 0;
+}
