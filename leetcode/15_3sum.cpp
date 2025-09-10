a6618fcddd49b498cfcd95eee20650a69f8a9f59
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-10 18:21:40 +0000

    three sum code update

--- leetcode/15_3sum.cpp
+++ leetcode/15_3sum.cpp
@@ -25,3 +25,46 @@ Input: nums = [0,0,0]
 Output: [[0,0,0]]
 Explanation: The only possible triplet sums up to 0.
 */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+    public:
+    vector<vector<int>> threeSum(vector<int>& nums){
+        vector<vector<int>> result;
+        int n = nums.size();
+
+        // Sort the array to use two pointers and avoid duplicates easily
+        sort(nums.beign(), nums.end());
+
+        for(int i=0;i<n;i++){
+            // Skip duplicate elements for i
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int target = -nums[i];
+            int left = i+1;
+            int right = n-1;
+
+            while(left<right){
+                int sum = nums[left]+nums[right];
+                if(sum==target){
+                    result.push_back(nums[i], nums[left], nums[right]);
+                    
+                    // skip duplicates for left pointer
+                    while(left<right && nums[left]==nums[left-1]) left++;
+
+                    // skip duplicates for right pointer
+                    while(left<right && nums[right]==nums[right+1]) right--;
+
+                    left++;
+                    right--;
+                }
+                else if(sum<target) < left++;
+                else right--;
+            }
+        }
+        return result;
+    }
+};
