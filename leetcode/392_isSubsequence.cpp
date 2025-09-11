a6090aa07b409716a9e898c7f9cc98e2043bc6c2
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-11 14:36:04 +0000

    leetcode 392 code update

--- leetcode/392_isSubsequence.cpp
+++ leetcode/392_isSubsequence.cpp
@@ -17,3 +17,21 @@ Constraints:
 0 <= t.length <= 104
 s and t consist only of lowercase English letters.
 */
+
+#include<iostream>
+using namespace std;
+
+class Solution {
+    public:
+    bool isSubsequence(string s, string t){
+        int i=0,j=0;
+        while(i<s.size() && j<t.size()){
+            if(s[i]==t[j]){
+                i++;
+            }
+            j++;
+        }
+
+        return i==s.size();
+    }
+};
