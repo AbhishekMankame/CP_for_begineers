fc0e893663468c325de72046e187175106ef8ed3
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-11 14:52:04 +0000

    leetcode 392 follow up question update

---
+++ leetcode/392_follow_up_question.cpp
@@ -0,0 +1,3 @@
+// Leetcode 392 - Is Subsequence --> Follow up question
+
+/*Suppose there are lots of incoming `s`, say `s1, s2, .. sk` where `k>=10^9`, and you want to check one by one to see if `t` has its subsequence. In this scenario, how would you change the code?*/
