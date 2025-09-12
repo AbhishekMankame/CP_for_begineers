9c253d9e7de61dede6df56c5333567e1cbed5957
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-12 18:55:11 +0000

    code update

--- leetcode/32_longest_valid_parenthesis.cpp
+++ leetcode/32_longest_valid_parenthesis.cpp
@@ -18,3 +18,29 @@ Input: s = ""
 Output: 0
 
 */
+
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int longestValidParentheses(string s){
+    stack<int> tk;
+    stk.push(-1); // base index for valid substring
+    int maxLen=0;
+
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='('){
+            stk.push(i);
+        }
+        else {
+            stk.pop();
+            if(stk.empty()){
+                stk.push(i); // reset base
+            }
+            else {
+                maxLen = max(maxLen, i-stk.top());
+            }
+        }
+    }
+    return maxLen;
+}
