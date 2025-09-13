f961c2406de9d9c7d03e2683d7598dd49c5e1bc4
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-13 18:21:48 +0000

    leetcode 3541 question update

---
+++ leetcode/3541_Find Most Frequent Vowel and Consonant.cpp
@@ -0,0 +1,15 @@
+// Leetcode 3541 - Find Most Frequent Vowel and Consonant
+
+/*You are given a string s consisting of lowercase English letters (`a` to `z`).
+
+Your task is to:
+- Find the vowel (one of `a`, `e`, `i`, `o`, `u`) with the maximum frequency.
+- Find the consonant (all other letters excluding vowels) with the maximum frequency.
+
+Return the sum of the two frequencies.
+
+Note: If mutliple vowels or consonants have the same maximum frequency, you may choose any one of them. If there are no vowels or no consonants in the string, consider their frequency as 0.
+
+The frequency of a letter `x` is the number of times it occurs in the string.
+
+*/
