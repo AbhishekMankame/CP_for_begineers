dbcb422206a1e029f8afd68a4882707180a8b566
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-14 20:13:17 +0000

    complete code update

--- Graphs/dfs_simple_example.cpp
+++ Graphs/dfs_simple_example.cpp
@@ -17,3 +17,25 @@ void dfs(int node, const vector<vector<int>>& adj, vector<bool>& visited){
         }
     }
 }
+
+int main() {
+    int n,e;
+    cout << "Enter number of nodes and edges: ";
+    cin >> n >> e;
+
+    vector<vector<int>> adj(n);
+    cout << "Enter edges (u v):" << endl;
+    for(int i=0;i<e;i++){
+        int u,v;
+        cin>>u>>v;
+        // Assuming undirected graph
+        adj[u].push_back(u);
+        adj[v].push_back(v);
+    }
+
+    vector<bool> visited(n, false);
+    cout << "DFS traversal starting from node 0:" << endl;
+    dfs(0, adj, visited);
+    cout << endl;
+    return 0;
+}
