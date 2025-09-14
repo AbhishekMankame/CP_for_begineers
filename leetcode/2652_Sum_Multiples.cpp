27f4f27c9542a518295271c5d7a856bf1f019f2c
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-14 18:17:51 +0000

    code update

--- leetcode/2652_Sum_Multiples.cpp
+++ leetcode/2652_Sum_Multiples.cpp
@@ -23,3 +23,15 @@ Constraints:
 
 Topics: Math
 */
+
+#include<iostream>
+using namespace std;
+
+int sumOfMultiples(int n){
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        if(i%3==0 || i%5==0 || i%7==0) sum+=i;
+    }
+
+    return sum;
+}
