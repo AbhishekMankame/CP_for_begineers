38f28a731f53a7b98b5cc4e58e47031efb1a1b79
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-14 20:19:37 +0000

    details comment update

--- Graphs/dfs_simple_example.cpp
+++ Graphs/dfs_simple_example.cpp
@@ -6,14 +6,14 @@ using namespace std;
 
 // Recursive DFS function
 void dfs(int node, const vector<vector<int>>& adj, vector<bool>& visited){
-    visited[node] = true;
+    visited[node] = true;   // Mark the current node as visited
 
     // process node: print current node
-    cout << node << " ";
+    cout << node << " ";    // Process node: here, simply print it
 
-    for(int nbr:adj[node]){
-        if(!visited[nbr]){
-            dfs(ndr, adj, visited);
+    for(int nbr:adj[node]){ // Iterate through all neighbors of 'node'
+        if(!visited[nbr]){  // If neighbour is not visited
+            dfs(ndr, adj, visited); // Recursively call DFS on neighbor
         }
     }
 }
@@ -23,19 +23,19 @@ int main() {
     cout << "Enter number of nodes and edges: ";
     cin >> n >> e;
 
-    vector<vector<int>> adj(n);
+    vector<vector<int>> adj(n); // Adjacency list to represent the graph
     cout << "Enter edges (u v):" << endl;
     for(int i=0;i<e;i++){
         int u,v;
         cin>>u>>v;
         // Assuming undirected graph
-        adj[u].push_back(u);
-        adj[v].push_back(v);
+        adj[u].push_back(u);    // Add edge u->v
+        adj[v].push_back(v);    // Add edge v->u (undirected graph)
     }
 
-    vector<bool> visited(n, false);
+    vector<bool> visited(n, false); // Track visited nodes
     cout << "DFS traversal starting from node 0:" << endl;
-    dfs(0, adj, visited);
+    dfs(0, adj, visited);   // Start DFS from node 0
     cout << endl;
     return 0;
 }
