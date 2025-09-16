81dc427c171e6c9cdced76b8ad99424ab6ac9077
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-16 17:49:08 +0000

    code update

--- leetcode/3_longest_substring_without_repeating_characters.cpp
+++ leetcode/3_longest_substring_without_repeating_characters.cpp
@@ -25,3 +25,19 @@ Constraints:
 
 Topics: Hash Table, String, Sliding Window
 */
+
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+int lengthOfLongestSubstring(string s){
+    unordered_map<char, int> seen;
+    int ret=0, slow=0, n=s.size();
+    for(int fast=0;fast<n;fast++){
+        if(seen.count(s[fast])!=0)  slow=max(slow, seen[s[fast]]+1);
+        seen[s[fast]] = fast;
+        ret = max(ret, fast-slow+1);
+    }
+
+    return ret;
+}
