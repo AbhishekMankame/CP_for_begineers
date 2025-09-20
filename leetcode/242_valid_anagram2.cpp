42654dc2832f62621e3b3399e528b863df90a7f3
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-20 17:35:09 +0000

    leetcode 242 --> valid anagram optimzed approach update

---
+++ leetcode/242_valid_anagram2.cpp
@@ -0,0 +1,11 @@
+// Leetcode 242 --> Valid Anagram
+
+/*Optimized Approach
+- Here in the base case we will compare the size, if the size(length) of both the strings does not match then we can directly return false.
+- If the size(length) of both the strings are same, then we can initialize a hash array (let's name it as count) of size 26 to count the occurence of all the characters in the given string.
+- We can run the for loop for first string 's' and increase the count of that particular character.
+- Then run another for loop for second string 't' and decrease the count of that particular character.
+- Run the loop for hash array 'count', and check if any of the characters count is not equal to 0.
+- If count is not equal to 0 then we can return false (means given two strings are not anagram), else return false (means given two strings are anagram)
+
+*/
