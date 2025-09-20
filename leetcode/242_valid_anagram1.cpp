341b5172e9b241de214b1325938c38277127f8eb
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-20 17:13:45 +0000

    leetcode 242 question update

---
+++ leetcode/242_valid_anagram1.cpp
@@ -0,0 +1,19 @@
+// Leetcode 242 --> Valid Anagram
+/*Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+Note: Anagram is a word or phrase formed by rearranging the letters of a different word or a phrase, using all the original letters exactly once.
+
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+
+Constraints:
+- 1 <= s.length, t.length <= 5*10^4
+- s and t consist of lowercase English letters.
+
+Topics: Hash Table, String, Sorting
+*/
