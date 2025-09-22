65f97f151c31e8ba2437f9a383f6876e30daa19f
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-09-22 18:30:19 +0000

    question commit

--- leetcode/75_sorted_array.cpp
+++ leetcode/75_sorted_array.cpp
@@ -1,4 +1,7 @@
 // Leetcode 75 --> Sort Colors
 /*Given an array nums with n objects colored red, white, blue, sort them in-place so that the objects of the same color are adjacent, with the colors in the order red, white, and blue.
 
-We will use the integers 0, 1 and 2 to represent the color red, white and blue, respectively.*/
+We will use the integers 0, 1 and 2 to represent the color red, white and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+*/
