a92913afbaa865fd3e1d19c76d78a47d10275f38
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-01 18:29:18 +0000

    question update

--- leetcode/1518_water_bottles.cpp
+++ leetcode/1518_water_bottles.cpp
@@ -10,4 +10,16 @@ Input: numBottles = 9, numExchange = 3
 Output: 13
 Explanation: You can exchange 3 empty bottles to get 1 full water bottle.
 Number of water bottles you can drink: 9 + 3 + 1 = 13.
+
+Example 2:
+Input: numBottles = 15, numExchange = 4
+Output: 19
+Explanation: You can exchange 4 empty bottles to get 1 full water bottle. 
+Number of water bottles you can drink: 15 + 3 + 1 = 19.
+
+Constraints:
+1<=numBottles<=100
+2<=numExchange<=100
+
+Topics: Math, Simulation
 */
