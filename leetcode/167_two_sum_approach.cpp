ffd764317bb848d16d3c73793cbd75ae33d93e9b
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-03 18:29:22 +0000

    code update

---
+++ leetcode/167_two_sum_approach.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int st = 0, end = numbers.size() - 1; // st starts from 0, end starts from the last element
+        while (st < end) {
+            int sum = numbers[st] + numbers[end]; // sum of the current elements
+            if (sum == target) {
+                return {st + 1, end + 1}; // return 1-based indices
+            } else if (sum < target) {
+                st++; // increase the starting index if the sum is less than the target
+            } else {
+                end--; // decrease the ending index if the sum is greater than the target
+            }
+        }
+
+        return {-1, -1}; // If no valid pair is found
+    }
+};
