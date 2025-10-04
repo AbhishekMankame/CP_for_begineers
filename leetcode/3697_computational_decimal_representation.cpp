6fef6077529582e5e29d1ee4572f902009cba7e1
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-04 18:33:15 +0000

    question update

--- leetcode/3697_computational_decimal_representation.cpp
+++ leetcode/3697_computational_decimal_representation.cpp
@@ -12,5 +12,18 @@ Return an array containing these base-10 components in descending order.
 Example 1:
 Input: n=37
 Output: [500, 30, 7]
-Explanation: We can express 537 as 500+30+7. It is impossible to express 537 as a sum using fewer than 3 base-10
+Explanation: We can express 537 as 500+30+7. It is impossible to express 537 as a sum using fewer than 3 base-10.
+
+Example 2:
+Input: n=102
+Output: [100, 2]
+Explanation: We can express 102 as 100+2. 102 is not a base-10 component, which means 2 base-10 components are needed.
+
+Example 3:
+Input: n=6
+Output: [6]
+Explanation: 6 is a base-10 component.
+
+Constraints:
+1 <= n <= 10^9
 */
