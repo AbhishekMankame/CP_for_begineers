c109fd372558d8468c4b2a23dde2e822c2ed56c5
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-07 18:32:10 +0000

    code update

--- codeforces/A-Raising_bacteria.cpp
+++ codeforces/A-Raising_bacteria.cpp
@@ -20,3 +20,19 @@ Output: 2
 
 Problem tags: bitmasks, *1000
 */
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int ans=0;
+
+    while(n){
+        if(n&1) ans++;
+        n>>=1;
+    }
+    cout<<ans<<endl;
+    return 0;
+}
