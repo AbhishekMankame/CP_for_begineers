faa911d7ee26b13e6e06d658e47be0a7f2963153
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-08 18:18:00 +0000

    example update

--- leetcode/2300_successful_pairs_of_spells_and_potions.cpp
+++ leetcode/2300_successful_pairs_of_spells_and_potions.cpp
@@ -8,4 +8,12 @@ You are also givern an integer 'success'. A spell and potion pair is considered 
 
 Return an integer array pairs of length n where pairs[i] is the number of potions that will form a successful pair with the ith spell.
 
+Example 1:
+Input: spells = [5,1,3], potions = [1,2,3,4,5], sucess = 7
+Output: [4,0,3]
+Explanation:
+- 0th spell: 5 * [1,2,3,4,5] = [5,10,15,20,25]. 4 pairs are successful.
+- 1st spell: 1 * [1,2,3,4,5] = [1,2,3,4,5]. 0 pairs are successful.
+- 2nd spell: 3 * [1,2,3,4,5] = [3,6,9,12,15]. 3 pairs are succesul.
+Thus, [4,0,3] is returned.
 */
