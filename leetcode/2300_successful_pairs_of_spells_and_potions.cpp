375ed1d90335fe5a0a7c837b2dc6467876991d22
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-08 18:08:00 +0000

    leetcode 2300 question update

---
+++ leetcode/2300_successful_pairs_of_spells_and_potions.cpp
@@ -0,0 +1,11 @@
+// Leetcode 2330 - Successful Pairs of Spells and Potions
+// Difficulty: Medium
+
+/*
+You are givern two positive integer arrays 'spells' and 'potions', of length 'n' and 'm' respectively, where 'spells[i]' represents the strength of the ith spell and 'positions[j]' represents the strength of the jth potion.
+
+You are also givern an integer 'success'. A spell and potion pair is considered successful if the product of their strengths is at least success.
+
+Return an integer array pairs of length n where pairs[i] is the number of potions that will form a successful pair with the ith spell.
+
+*/
