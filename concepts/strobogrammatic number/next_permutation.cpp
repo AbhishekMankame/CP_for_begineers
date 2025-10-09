e2a7a0c20161ca8109c0a8d3390e9ad3682e3ed0
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-09 18:29:12 +0000

    next_permutation topics update

---
+++ concepts/strobogrammatic number/next_permutation.cpp
@@ -0,0 +1,3 @@
+// What is next_permutation?
+/*next_permutation is a function in the C++ STL (in the <algorithm>
+header) that rearranges the elements in a given range to the next lexicographically greater permutation of the elements.*/
