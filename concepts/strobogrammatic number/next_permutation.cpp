4da6b9dbed967ba135528471b4f80a3dd7142daa
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-09 18:35:20 +0000

    description update

--- concepts/strobogrammatic number/next_permutation.cpp
+++ concepts/strobogrammatic number/next_permutation.cpp
@@ -1,3 +1,6 @@
 // What is next_permutation?
 /*next_permutation is a function in the C++ STL (in the <algorithm>
-header) that rearranges the elements in a given range to the next lexicographically greater permutation of the elements.*/
+header) that rearranges the elements in a given range to the next lexicographically greater permutation of the elements.
+- If such a permutation exists, it transforms the range into the next permutation and returns true.
+- If the range is already in the highest possible permutaion (i.e, sorted in descending order), it rearranges the elements into the lowest possible permutation (sorted in ascending ordre) and returns false.
+*/
