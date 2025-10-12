4e2cc75bd85bdc29d4faee4d5e7146c6d08a0e3d
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-12 09:34:49 +0000

    function code update

--- gfg/prefix_sum/two_pointers_optimized.cpp
+++ gfg/prefix_sum/two_pointers_optimized.cpp
@@ -4,3 +4,26 @@ We take two pointers, one representing the first element and other representing 
 If their sum is smaller than X then we shift the left pointer to right or if their sum is greater than X then then we shift right pointer to left, in order to get closer to the sum.
 We keep moving the pointers until we get the sum as X.
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int isPairSum(int A[], int N, int X){
+    // represents first pointer
+    int i=0;
+
+    // represents second pointer
+    int j=N-1;
+
+    while(i<j){
+        // If we find a pair
+        if(A[i]+A[j]==X) return 1;
+
+        // If sum of elements at current pointers is less, we move towards higher value by doing i++
+        else if (A[i]+A[j]<X) i++;
+
+        // If sum of elements at current pointers is more, we move towards lower values by doing j--
+        eles j--;
+    }
+    return 0;
+}
