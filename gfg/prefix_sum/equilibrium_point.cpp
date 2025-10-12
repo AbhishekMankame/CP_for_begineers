773d685e0023cf464e26cb670c75e8c33520632b
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-12 07:04:14 +0000

    equilibrium point ka definition

---
+++ gfg/prefix_sum/equilibrium_point.cpp
@@ -0,0 +1,7 @@
+// Equilibrium index of an array using prefix-sum:
+
+/*Here the idea is to take the prefix sum of the array twice. Once from the front end of the array and antoher from the back end of the array.
+After taking both prefix sums run a loop and check for some i if both the prefix sum from one array is equal to prefix sum from the second array then that point can be considered as the Equilibrium point.
+
+
+*/
