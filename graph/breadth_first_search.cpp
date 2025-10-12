00ce54ef81101820d4b676b83fcd0a42fd22aee6
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-12 13:39:03 +0000

    BFS code update

--- graph/breadth_first_search.cpp
+++ graph/breadth_first_search.cpp
@@ -7,5 +7,42 @@
 using namespace std;
 
 void bfs(int start, vector<vector<int>>& graph){
-    
+    int n = graph.size();
+    vector<bool> visited(n,false);
+    queue<int> q;
+    visited[start]=true;
+    q.push(start);
+
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        cout<<node<<" ";
+        for(int neighbor:graph[node]){
+            if(!visited[neighbor]){
+                visited[neighbor]=true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
+int main() {
+    int n=5;
+    vector<vector<int>> graph(n);
+
+    // Edges (undirected)
+    graph[0].push_back(1);
+    graph[1].push_back(0);
+    graph[0].push_back(2);
+    graph[2].push_back(0);
+    graph[1].push_back(3);
+    graph[3].push_back(1);
+    graph[2].push_back(4);
+    graph[4].push_back(2);
+
+    cout << "BFS starting from node 0: ";
+    bfs(0, graph);
+    cout<<endl;
+
+    return 0;
 }
