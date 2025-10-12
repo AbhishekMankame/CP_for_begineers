1c6b0956b2eb10c504711dddf40552e186d02bec
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-12 12:32:07 +0000

    graph introduction

---
+++ Graphs/intro_and_representation.cpp
@@ -0,0 +1,10 @@
+/*
+Graph
+- A graph is a collection of nodes (called vertices) connected by edges.
+- Eg. Social networks (people are nodes, friendships are edges).
+
+Graphs can be:
+- Unidirected: Edges have no direction (friendship is mutual).
+- Directed: Edges have direction (like Twitter follow)
+
+*/
