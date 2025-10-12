58ae4601fad8f5f9095740b8dac12a71516f6b65
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-12 12:47:18 +0000

    graph representation code update

--- Graphs/intro_and_representation.cpp
+++ Graphs/intro_and_representation.cpp
@@ -11,3 +11,38 @@ Graphs can be:
 
 // Representation of a graph
 // A simple way is adjacency list, which stores for each node the list of nodes connected to it.
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    int n = 5; // number of nodes (0 to 4)
+
+    // Create adjacency list
+    vector<vector<int>> graph(n);
+
+    // Add edges (undirected)
+    graph[0].push_back(1);
+    graph[1].push_back(0);
+
+    graph[0].push_back(2);
+    graph[2].push_back(0);
+
+    graph[1].push_back(3);
+    graph[3].push_back(1);
+
+    graph[2].push_back(4);
+    graph[4].push_back(2);
+
+    // Print adjacency list
+    for(int i=0;i<n;i++){
+        cout << i << ": ";
+        for(int node : graph[i]){
+            cout << node << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
