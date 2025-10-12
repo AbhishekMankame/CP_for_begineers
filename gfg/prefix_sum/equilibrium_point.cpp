0b1c7bb8c563d78d6c90779adad76eb8d964c4d9
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-12 08:03:18 +0000

    function update

--- gfg/prefix_sum/equilibrium_point.cpp
+++ gfg/prefix_sum/equilibrium_point.cpp
@@ -9,3 +9,37 @@ Follow the given steps to solve the problem:
 - If any such index is found then return that index
 - Else return -1
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int equilibrium(int a[], int n){
+    if(n==1) return 0;
+    int forward[n]={0};
+    int backward[n]={0};
+    int forward[0]=a[0];
+    int rev[n] = {0};
+
+    forward[0]=a[0];
+    rev[n-1]=a[n-1];
+
+    // Taking the prefix sum from the front of the array
+    for(int i=1;i<n;i++){
+        forward[i]=forward[i-1]+a[i];
+    }
+
+    // Taking the prefix sum from the back end of the array
+    for(int i=n-2;i>=0;i--){
+        rev[i]=rev[i+1]+a[i];
+    }
+
+    // Checking if forward prefix sum is equal to rev prefix sum
+    for(int i=0;i<n;i++){
+        if(forward[i]==rev[i]) return i;
+    }
+
+    return -1;
+
+    // If you want all the points of equilibrium create vector and push all equilibrium points in it and return the vector
+
+}
