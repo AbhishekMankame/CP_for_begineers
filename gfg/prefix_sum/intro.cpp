41699b481f95589e7c33be6fb5b5f0c06f162d47
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-12 06:08:11 +0000

    code and output update

--- gfg/prefix_sum/intro.cpp
+++ gfg/prefix_sum/intro.cpp
@@ -9,5 +9,32 @@ prefixSum[0] = 10,
 prefixSum[1] = prefixSum[0] + arr[1] = 30,
 prefixSum[2] = prefixSum[1] + arr[2] = 40 and so on.
 
+To fill the prefix sum array, we run through index 1 to last and keep on adding the present element with the previous value in the prefix sum array.
 
 */
+
+#include<iostream>
+using namespace std;
+
+void fillPrefixSum(int arr[], int n, int prefixSum[]){
+    prefixSum[0]=arr[0];
+
+    // Adding present element with previous element
+
+    for(int i=1;i<n;i++){
+        prefixSum[i] = prefixSum[i-1] + arr[i];
+    }
+}
+
+int main() {
+    int arr[] = {10,4,16,20};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int prefixSum[n];
+
+    fillPrefixSum(arr,n,prefixSum);
+    for(int i=0;i<n;i++){
+        cout<<prefixSum[i]<<" ";
+    }
+}
+
+// Output: 10 14 30 50
