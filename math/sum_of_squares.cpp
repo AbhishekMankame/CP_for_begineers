7ee17c41ba5d7a96a2ad4bdca1c4efe7c2b75627
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-13 17:29:03 +0000

    naive approach code and complexity update

--- math/sum_of_squares.cpp
+++ math/sum_of_squares.cpp
@@ -12,3 +12,19 @@ Explanation: 1^2 + 2^2 + 3^2 + 4^2 + 5^2 + 6^2 + 7^2 + 8^2 = 204
 
 */
 
+// Naive approach: A naive approach will be o run a loop from 1 to n and sum up all the squares
+
+#include<iostream>
+using namespace std;
+
+int sumation(int n){
+    int sum=0;
+    for(int i=1;i<=n;i++){
+        sum += (i*i);
+    }
+
+    return sum;
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(1), since no extra space has been taken
