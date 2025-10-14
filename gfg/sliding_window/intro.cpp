4cd75d51ffb269885481adf6dbc29f64212cd873
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-14 18:04:53 +0000

    sliding window intro

---
+++ gfg/sliding_window/intro.cpp
@@ -0,0 +1,8 @@
+// Sliding Window Technique
+
+/*
+Sliding window technique is a method used to solve problems that involve subarray or substring or window.
+- Instead of repeatedly iterating over the same elements. the sliding window maintains a range (or "window") that moves step-by-step through data, updating results incrementally.
+- The main idea is to use the results of previous window to do computations for the next window.
+
+*/
