233bfb5d669ed360a35bcdf3857ad8994512a218
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-16 19:13:26 +0000

    first and last occurrneces of element in sorted array intro

---
+++ concepts/binary_search/first_and_last_occurence_of_element.cpp
@@ -0,0 +1,9 @@
+// First and last occurrences of element in sorted array
+
+/* 
+To find the first and last occurrences of an element in a sorted array efficiently, we can use binary search.
+
+Why binary search?
+- Since the array is sorted, binary search can find positions in O(log n) time.
+- Using two variations of binary search we can find the first and last occurrences sepeartely.
+*/
