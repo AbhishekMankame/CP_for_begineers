77384216604fae92295232a2c3c2706ca891d1cf
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-16 18:24:20 +0000

    code update

--- leetcode/2149_rearrange_array_elements_by_sign.cpp
+++ leetcode/2149_rearrange_array_elements_by_sign.cpp
@@ -36,3 +36,25 @@ It is not required to do the modification in-place.
 Topics: Array, Two Pointers, Simulation
 
 */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> rearrangeArray(vector<int>& nums){
+    int n=nums.size();
+    vector<int> ans(n,0);
+    int pos=0, neg=1;
+    for(int i=0;i<n;i++){
+        if(nums[i]>0){
+            ans[pos]=nums[i];
+            pos=pos+2;
+        }
+        else{
+            ans[neg]=nums[i];
+            neg=neg+2;
+        }
+    }
+    return ans;
+}
