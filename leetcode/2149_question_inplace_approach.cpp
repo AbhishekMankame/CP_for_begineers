a78885e873094db42ac1c1e2655a61a7f142e176
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-16 18:43:07 +0000

    code update

---
+++ leetcode/2149_question_inplace_approach.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+vector<int> rearrangeArray(vector<int>& nums){
+    int n=nums.size();
+    int i=0; // even index pointer (expect positive)
+    int j=1; // odd index pointer (expect negative)
+
+    while(i<n && j<n){
+        // If nums[i] is positive and nums[j] is negative, move on
+        if(nums[i]>0){
+            i+=2;
+        }
+
+        // If nums[j] is negative and nums[i] is positive, move on
+        else if(nums[i]<0){
+            j+=2;
+        }
+        eles {
+            // Swap nums[i] (negative in even place) with nums[j] (positive in odd place)
+            swap(nums[i],nums[j]);
+            i+=2;
+            j+=2;
+        }
+    }
+    return nums;
+}
