f047867e4646502d82fd609105107341a913e4e0
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-16 19:28:43 +0000

    code and complexity update

--- concepts/binary_search/first_and_last_occurence_of_element.cpp
+++ concepts/binary_search/first_and_last_occurence_of_element.cpp
@@ -50,3 +50,30 @@ int lastOccurrence(const vector<int> &nums, int target){
     }
     return result;
 }
+
+int main(){
+    vector<int> nums = {1,2,2,2,3,4,5};
+    int target=2;
+    int first=firstOccurrnece(nums,target);
+    int last=lastOccurrence(nums,target);
+    cout<<"First occurrence of "<<target<<" is at index: "<<first<<endl;
+    cout<<"Last occurrence of "<<target<<" is at index: "<<last<<endl;
+
+    return 0;
+}
+
+/*
+Time Complexity:
+- Each function ('firstOccurrence' and 'lastOccurrence') performs a binary search.
+- Binary search runs in O(log n) time, where n is the size of the array.
+- Since you run two seperate binary searches, total time is:
+        O(log n) + O(log n) = O(log n)
+    which simplifies to O(log n) overall
+
+Space Complexity:
+- The code uses a constant number of variables (low, high, mid, result).
+- No extra data structures proportional to input size are used.
+- So the space complexity is: O(1) (i.e., constant extra space)
+
+
+*/
