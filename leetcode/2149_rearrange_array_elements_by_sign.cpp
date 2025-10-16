d3fabd5a75b1433c28d47c4ce6d6a3bf64fbe63d
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-16 18:14:39 +0000

    leetcode 2149 question update

---
+++ leetcode/2149_rearrange_array_elements_by_sign.cpp
@@ -0,0 +1,16 @@
+// Leetcode 2149 - Rearrange Array Elements by Sign
+// Medium
+
+/*
+You are given a 0-indexed integer array 'nums' of even length consisting of an equal number of positive and negative integers.
+
+You should return the array of nums such the array follows the given conditions:
+1. Even consecutive pair of integers have opposite signs.
+2. For all integers with the same sign, the order in which they were present in nums is preserved.
+3. The rearranged array begins with a positive integer.
+
+Return the modified array after rearranging the elements to satisfy the aforementioned conditions.
+
+
+
+*/
