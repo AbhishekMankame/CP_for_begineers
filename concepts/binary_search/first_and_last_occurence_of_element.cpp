421b43d3663370c82aecb7186c99d9679cc31b15
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-16 19:17:50 +0000

    first occurrence code update

--- concepts/binary_search/first_and_last_occurence_of_element.cpp
+++ concepts/binary_search/first_and_last_occurence_of_element.cpp
@@ -7,3 +7,26 @@ Why binary search?
 - Since the array is sorted, binary search can find positions in O(log n) time.
 - Using two variations of binary search we can find the first and last occurrences sepeartely.
 */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int firstOccurrnece(const vector<int>& nums, int target){
+    int low=0, high=(int)nums.size()-1;
+    int result=-1;
+    while(low<=high){
+        int mid = low + (high-low)/2;
+        if(nums[mid]==target){
+            result=mid; // potential answer found
+            high = mid-1; // look for earlier occurrnece
+        }
+        else if(nums[mid]<target){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return result;
+}
