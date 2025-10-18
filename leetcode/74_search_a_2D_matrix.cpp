2d875ea2fdec4e54e2fd76e33242285493c532cb
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-18 18:13:25 +0000

    question update

--- leetcode/74_search_a_2D_matrix.cpp
+++ leetcode/74_search_a_2D_matrix.cpp
@@ -9,4 +9,20 @@ Given an integer 'target', return 'true' if 'target' is in 'matrix' or 'false' o
 
 You must write a solution in O(log(m*n)) time complexity.
 
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target=3
+Output: true
+
+Example 2:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+
+Constraints:
+- m == matrix.length
+- n == matrix[i].length
+- 1 <= m, n <= 100
+- -10^4 <= matrix[i][j], target <= 10^4
+
+Topics: Array, Binary Search, Matrix
+
 */
