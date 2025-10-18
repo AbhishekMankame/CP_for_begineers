aeb1e43c0179fd2f4581e4212c1de03a6c901094
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-18 18:17:24 +0000

    code update

--- leetcode/74_search_a_2D_matrix.cpp
+++ leetcode/74_search_a_2D_matrix.cpp
@@ -26,3 +26,19 @@ Constraints:
 Topics: Array, Binary Search, Matrix
 
 */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target){
+    int i=0,j=matrix[0].size()-1;
+    while(i<matrix.size() && j>=0){
+        if(matrix[i][j]==target) return true;
+        else if(matrix[i][j]>target) j--;
+        else i++;
+    }
+    return false;
+}
+
