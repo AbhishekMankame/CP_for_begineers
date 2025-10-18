901454581ccb207bfa4933544a8991b91ecd5636
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-18 18:29:37 +0000

    leetcode 728 question update

---
+++ leetcode/728_self_dividing_numbers.cpp
@@ -0,0 +1,12 @@
+// Leetcode 728 - Self Dividing Number
+// Easy
+
+/*
+A self-diving number is a number that is divisible by every digit it contains.
+- For example, 128 is a self-dividing number because 128%1==0, 128%2==0 and 128%==0.
+
+A self-dividing number is not allowed to contain the digit zero.
+
+Given two integers left and right, return a list of all the self-dividing numbers in the range [left,right] (both inclusive).
+
+*/
