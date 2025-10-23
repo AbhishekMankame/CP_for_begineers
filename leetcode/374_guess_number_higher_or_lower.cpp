78a4e196e3b3ff37c36d467936b1101aab62ae9b
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-23 18:51:35 +0000

    code update

--- leetcode/374_guess_number_higher_or_lower.cpp
+++ leetcode/374_guess_number_higher_or_lower.cpp
@@ -32,3 +32,17 @@ Constraints:
 Topics: Binary Search, Interactive
 
 */
+
+#include<iostream>
+using namespace std;
+
+int guessNumber(int n){
+    int left = 0, right = n;
+    while(left<right){
+        int mid = left + (right - left)/2;
+        if(guess(mid)==0) return mid;
+        else if(guess(mid)==1) left = mid + 1;
+        else right = mid - 1;
+    }
+    return left;
+}
