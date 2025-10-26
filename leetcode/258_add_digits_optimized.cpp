cc6138daff8f499709f84a489b269ff296870551
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-26 18:06:15 +0000

    code and complexity update

--- leetcode/258_add_digits_optimized.cpp
+++ leetcode/258_add_digits_optimized.cpp
@@ -8,3 +8,14 @@ digits(num) = 0 if num==0
 This works because the repeated sum of digits modulo 9 gives the same result as the digital root.
 
 */
+
+#include<iostream>
+using namespace std;
+
+int addDigits(int num){
+    if(num==0) return 0;
+    return 1+(num-1)%9;
+}
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
