0b99e91608f49bfd2b3e0206a80bcf4a0330aff4
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-10-29 23:36:45 +0530

    codeforces 1829B question update

---
+++ 1829_B:Blank Space.cpp
@@ -0,0 +1,19 @@
+// Codeforces 1829 B - Blank Space
+// Problem tags: implementation, 800
+
+/*
+You are given a binary array a of n elements, a binary array  is an array consisting only of 0s and 1s.
+
+A blank space is a segment of consecutive elements consisting of only 0s.
+
+Your task is to find the length of the longest blank space.
+
+Input:
+The first line contains a single integer t (1<=t<=1000) - the number of test cases.
+The first line of each test case contains a single integer n (1<=n<=100) - the length of the array.
+The second line of each test case contains n space-seperated integers ai (0 <= ai <= 1) - the elements of the array.
+
+Output:
+For each test case, output a single integer - the length of the longest blank space.
+
+*/
