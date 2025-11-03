801f2c42f051d3cd40cdab9ade4d2c19282b3abc
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-03 06:45:27 +0000

    sorting introduction

---
+++ Sorting/bubble sort/intro.cpp
@@ -0,0 +1,17 @@
+/*
+What is Sorting?
+Sorting algorithm take unsorted array in random order, and output that number in increasing order or in decreasing order.
+
+Example:
+Unsorted Array
+a = {10, 12, 5, 4, 1, 3, 2};
+
+Sorted Array in Increasing Order
+a = {1, 2, 3, 4, 5, 10, 12}
+
+Sorted Array in Decreasing Order
+a = {12, 10, 5, 4, 3, 2, 1}
+
+Bubble Sort
+Key Idea: Take larger element to the end by repeatedly swapping the adjacent elements.
+*/
