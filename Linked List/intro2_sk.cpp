4957011dff7120d592a628b3587c01d74f8dad2f
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-04 19:10:52 +0000

    list update

--- Linked List/intro2_sk.cpp
+++ Linked List/intro2_sk.cpp
@@ -13,4 +13,14 @@ public:
     // The above is an individual node class
     
     // Note: Kisi bhi ek linked list ke liye, 2 special chize hai joh store honi chahiye: Head Pointer and tail pointer
-}
+};
+
+class List {
+    Node* head; // Note: Joh bhi values public ya protected declare kiya hai, woh saare ke saare private member hoti hai
+    Node* tail;
+
+public:
+    List() {    // this will be non parameterized constructor: isme koi parameter nahi aayegi
+        head = tail = NULL;
+    }
+};
