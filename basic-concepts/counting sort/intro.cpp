7f85d57610cefddace3783f995220d5deaa94923
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-04 14:42:49 +0000

    counting sort intro

---
+++ basic-concepts/counting sort/intro.cpp
@@ -0,0 +1,12 @@
+/*
+
+What is Counting Sort? 
+
+Counting sort is a non-comparison-based sorting algorithm that works well when:
+- The elements are integers (or can be mapped to integers).
+- The range of input values (max - min) is not too large compared to the number of elements.
+It counts the number of occurrences of each element and then reconstructs the sorted array.
+
+
+
+*/
