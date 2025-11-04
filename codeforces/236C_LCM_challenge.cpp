20175acca98d215811f9bd1cd104253b4bd3f466
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-04 08:26:22 +0000

    236C LCM challenge question update

---
+++ codeforces/236C_LCM_challenge.cpp
@@ -0,0 +1,21 @@
+// 236 C --> LCM Challenge
+// time limit per test: 2 seconds
+// memory limit per test: 256 megabytes
+
+/*
+Some days ago, I learned the concept of LCM (least common multiple). I've played with it for several times and I want to make a big number with it.
+
+But I also don't want to use many numbers, so I'll choosed three positive integers (they don't have to be distinct) which are not greater than n. Can you help me to find the maximum possible least common multiple of these three integers?
+
+Input:
+The first line contains an integer n (1<=n<=10^6) - the n mentioned in the statement.
+
+Output:
+Print a single integer - the maximum possible LCM of three not necessarily distinct positive integers that are not greater than n.
+
+Example:
+Input: 9    Output: 504
+Input: 7    Output: 210
+
+
+*/
