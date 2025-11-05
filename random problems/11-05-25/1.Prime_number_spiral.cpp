6d5baa211b827746e1d33fd716a4c425fa34bdc7
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-05 18:02:34 +0000

    code update

--- random problems/11-05-25/1.Prime_number_spiral.cpp
+++ random problems/11-05-25/1.Prime_number_spiral.cpp
@@ -16,3 +16,44 @@ Prime numbers = {2, 3, 5, 7}
 Output: 4
 
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPrime(int n){
+    if(n<2) return false;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<int>> a(n, vector<int>(n));
+    int top=0,bottom=n-1,left=0,right=n-1;
+    int num = 1;
+
+    while(top<=bottom && left <= right){
+        for(int i=left;i<=right;i++) a[top][i]=num++;
+        top++;
+        for(int i=top;i<=bottom;i++) a[i][right]=num++;
+        right--;
+        for(int i=right;i>=left;i--) a[bottom][i]=num++;
+        bottom--;
+        for(int i=bottom;i>=top;i--) a[i][left]=num++;
+        left++;
+
+    }
+
+    int primeCount=0;
+    for(auto &row:a){
+        for(int x:row){
+            if(isPrime(x)) primeCount++;
+        }
+    }
+
+    cout<<primeCount<<endl;
+    return 0;
+}
