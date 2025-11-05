2e1906e8d4d4ea3c6edfc1bf2a5998914be0d26a
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-05 17:55:39 +0000

    prine spiral question update

---
+++ random problems/11-05-25/1.Prime_number_spiral.cpp
@@ -0,0 +1,18 @@
+// 1. Prime Number Spiral
+
+/*
+Given an integer n, generate an n*n matrix filled with numbers from 1 to n^2
+in a spiral order.
+Then, count how many prime numbers appear in the matrix.
+
+Example:
+Input: n = 3
+Matrix:
+1 2 3
+8 9 4
+7 6 5
+
+Prime numbers = {2, 3, 5, 7}
+Output: 4
+
+*/
