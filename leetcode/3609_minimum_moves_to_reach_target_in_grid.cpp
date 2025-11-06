ebfed3fe68df84a596086b6bef0b450572ae99fb
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-06 02:14:21 +0000

    question update

--- leetcode/3609_minimum_moves_to_reach_target_in_grid.cpp
+++ leetcode/3609_minimum_moves_to_reach_target_in_grid.cpp
@@ -21,4 +21,25 @@ The optimal path is:
 
 Thus, the minimum number of moves to reach (5,4) is 2.
 
+Example 2:
+Input: sx = 0, sy = 1, tx = 2, ty = 3
+Output: 3
+Explanation:
+The optimal path is:
+Move 1: max(0, 1) = 1. Increase the x-coordinate by 1, moving from (0, 1) to (0 + 1, 1) = (1, 1).
+Move 2: max(1, 1) = 1. Increase the x-coordinate by 1, moving from (1, 1) to (1 + 1, 1) = (2, 1).
+Move 3: max(2, 1) = 2. Increase the y-coordinate by 2, moving from (2, 1) to (2, 1 + 2) = (2, 3).
+Thus, the minimum number of moves to reach (2, 3) is 3.
+
+Example 3:
+Input: sx = 1, sy = 1, tx = 2, ty = 2
+Output: -1
+Explanation:
+It is impossible to reach (2, 2) from (1, 1) using the allowed moves. Thus, the answer is -1.
+
+Constraints:
+0 <= sx <= tx = 10^9
+0 <= sy <= ty <= 10^9
+
+Topics: Math
 */
