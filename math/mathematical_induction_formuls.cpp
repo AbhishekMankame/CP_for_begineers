a3ced96a8ebcb9a4c3a63c3109f904a27885c798
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-08 03:45:34 +0000

    ap and sum of power of 2

--- math/mathematical_induction_formuls.cpp
+++ math/mathematical_induction_formuls.cpp
@@ -9,3 +9,14 @@ double geometricSum(double a, double r, int n){
     if(r==1) return a*n; // special case
     return a*(pow(r,n)-1)/(r-1); 
 }
+
+// Arithmetic Progression (AP)
+double aritmeticSum(double a, double b, int n){
+    return (n/2.0)*(2*a+(n-1)*d);
+}
+
+// Sum of power of 2
+long long powerOfTwoSum(int n){
+    return (1LL << (n+1))-1; // 2^(n+1)-1
+}
+
