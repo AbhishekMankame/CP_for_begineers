49248ef32ea8db7c787d2a172435301a50e37226
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-11 18:27:17 +0000

    strobogrammatic number intro

---
+++ leetcode/246_strobogrammatic_number.cpp
@@ -0,0 +1,11 @@
+// Leetcode 246 - Strobogrammatic Number
+// Easy
+
+/*
+A strobogrammatic number is a number that looks the same when rotated 180 degrees.
+For example, 69 and 101 are strobogrammatic numbers, but 123 is not. The valid digits for a strobogrammatic number are: 0, 1, 6, 8, and 9.
+
+To check if a number is strobogrammatic in C++, you can follow these steps:
+
+
+*/
