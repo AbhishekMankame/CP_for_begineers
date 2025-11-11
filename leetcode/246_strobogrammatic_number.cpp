69a253c8e07093d0c0a5a16c4ce33cbbbf63ec5a
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-11 18:44:06 +0000

    time complexity update

--- leetcode/246_strobogrammatic_number.cpp
+++ leetcode/246_strobogrammatic_number.cpp
@@ -56,3 +56,13 @@ int main() {
 
     return 0;
 }
+
+/*
+Time Complexity:
+The function isStrobogrammatic checks the digits of the number from both ends towards the center. It compares each pair of digits at positions left and right and verifies whether they form a valid strobogrammatic pair.
+1. Iterating through the digits:
+- The function uses two pointers, left starting from 0 and right starting from num.size()-1. These pointers move towards the center of the string.
+- In the worst case, the function will compare all the digits once, which means it will iterate through the string half the length. If the number has n digits, the loop will run n/2 times.
+- Therefore, the time complexity is O(n), where n is the length of the string.
+
+*/
