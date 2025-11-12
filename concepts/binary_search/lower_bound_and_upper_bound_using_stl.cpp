302a55ea4570f7bf36a39db074230affc96eeeda
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-12 16:25:42 +0000

    lower bound definition

--- concepts/binary_search/lower_bound_and_upper_bound_using_stl.cpp
+++ concepts/binary_search/lower_bound_and_upper_bound_using_stl.cpp
@@ -1,6 +1,12 @@
 // Lower bound and upper bound using STL 
 
 /*
+Lower Bound - In C++, the std::lower_bound() function is used to find the position of the first element in the sorted array that is greater than or equal to a given value.
+In other words, it returns an iterator pointing to the smallest element in the range that is not less than the target value.
+If all elements in the range are smaller than the target, it returns an iterator pointing to the end of the range. This function is commonly used to find the insertion position of an element in a sorted array while maintaing order, or to locate the starting index of a specific value.
+Internally, it uses binary search, so its time complexity is O(log n) and space complexity is O(1).
+
+
 lower_bound: Finds the first position where value could be inserted without violating order. (First element >= target)
 upper_bound: Find the first element strictly greater than target.
 
