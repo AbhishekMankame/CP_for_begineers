f1c9354e349858aed7788bb3e83555aa095275da
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-13 17:54:00 +0000

    second largest element in array brute force code

---
+++ basic-concepts/array/second_largest_element2.cpp
@@ -0,0 +1,29 @@
+// Code for Second largest element in sorted array
+
+// Brute Force approach
+
+#include<iostream>
+using namespace std;
+
+int secondLargestBruteForce(int arr[], int n){
+    if(n<2) return -1; // Not enough elements
+
+    int largest = arr[0];
+
+    // Find the largest element
+    for(int i=1;i<n;i++){
+        if(arr[i]>largest) largest=arr[i];
+    }
+
+    int secondLargest = -1;
+
+    // Find the largest element smaller than the largest
+    for(int i=0;i<n;i++){
+        if(arr[i]!=largest){
+            if(secondLargest==-1 || arr[i]>secondLargest){
+                secondLargest = arr[i];
+            }
+        }
+    }
+    return secondLargest;
+}
