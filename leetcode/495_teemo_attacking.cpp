dc8ccfd05bb8d2e27c76ca05649967ebbe6081d5
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-14 12:12:27 +0000

    question update

--- leetcode/495_teemo_attacking.cpp
+++ leetcode/495_teemo_attacking.cpp
@@ -9,4 +9,27 @@ If Teemo attacks again before the poison effect ends, the timer for it is reset,
 You are given a non-decreasing integer array 'timeSeries', where 'timeSeries[i]' denotes that Teemo attacks Ashe at second 'timeSeries[i]', and an integer 'duration'.
 Return the total number of seconds that Ashe is poisoned.
 
+Example 1:
+Input: timeSeries = [1, 4], duration = 2
+Output: 4
+Explanation: Teemo's attacks on Ashe go as follows:
+- At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+- At second 4, Teemo attacks, and Ashe is poisoned for seconds 4 and 5.
+Ashe is poisoned for seconds 1, 2, 4, and 5, which is 4 seconds in total.
+
+Example 2:
+Input: timeSeries = [1, 2], duration = 2
+Output: 3
+Explanation: Teemo's attacks, and Ashe go as follows:
+- At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+- At second 2 however, Teemo attacks again and resets the poison timer. Ashe is poisoned for seconds 2 and 3.
+Ashe is poisoned for seconds 1, 2, and 3, which is 3 seconds in total.
+
+Constraints:
+- 1 <= timeSeries.length <= 10^4
+- 0 <= timeSeries[i], duration <= 10^7
+- timeSeries is sorted in non-decreasing order.
+
+Topics: Array, Simulation
+
 */
