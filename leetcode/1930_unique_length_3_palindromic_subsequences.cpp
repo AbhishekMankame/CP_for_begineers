3d801562c25a2eca3ec06cdee9195bb1e9e4ac60
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-21 18:15:11 +0000

    question update

--- leetcode/1930_unique_length_3_palindromic_subsequences.cpp
+++ leetcode/1930_unique_length_3_palindromic_subsequences.cpp
@@ -10,4 +10,32 @@ A palindrome is a string that reads the same forwards and backwards.
 A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
 - For example, "ace" is a subsequence of "abcde".
 
+Example 1:
+Input: s = "aabca"
+Output: 3
+Explanation: The 3 palindromic subsequences of length 3 are:
+- "aba" (subsequence of "aabca")
+- "aaa" (subsequence of "aabca")
+- "aca" (subsequence of "aabca")
+
+Example 2:
+Input: s = "adc"
+Output: 0
+Explanation: There are no palindromic subsequences of length 3 in "adc".
+
+Example 3:
+Input: s = "bbcbaba"
+Output: 4
+Explanation: The 4 palindromic subsequences of length 3 are:
+- "bbb" (subsequence of "bbcbaba")
+- "bcb" (subsequence of "bbcbaba")
+- "bab" (subsequence of "bbcbaba")
+- "aba" (subsequence of "bbcbaba")
+
+Constraints:
+3 <= s.length <= 10^5
+s consists of only lowercase English letters
+
+Topics: Hash Table, String, Bit Manipulation, Prefix Sum
+
 */
