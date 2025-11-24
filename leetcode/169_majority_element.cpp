d72ed7ad6ebe1d694111748d9521657b42470fb5
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-24 18:26:23 +0000

    leetcode 169 question update

---
+++ leetcode/169_majority_element.cpp
@@ -0,0 +1,10 @@
+// 169 - Majority Element
+// Easy
+
+/*
+Given an array of size n, return the majority element.
+
+The majority element is the element that appears more than n/2 times.
+You may assume that the majority element always exists in the array.
+
+*/
