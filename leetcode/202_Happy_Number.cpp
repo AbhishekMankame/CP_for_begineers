cd469d615ebdc905e78d2a229a7727ec2d46b0ad
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-25 12:04:00 +0000

    question update

--- leetcode/202_Happy_Number.cpp
+++ leetcode/202_Happy_Number.cpp
@@ -10,4 +10,21 @@ A happy number is a number defined by the following process:
 - Those numbers for which this process ends in 1 are happy.
 
 Retur true if n is a happy, and false is not.
+
+Example 1:
+Input: n=19
+Output: true
+Explanation:
+1^2 + 9^2 = 82
+8^2 + 2^2 = 68
+6^2 + 8^2 = 100
+1^2 + 0^2 + 0^2 = 1
+
+Example 2:
+Input: n=2;
+Output: false
+
+Constraints: 1<=n<=2^31-1
+
+Topics: Hash Table, Math, Two Pointers
 */
