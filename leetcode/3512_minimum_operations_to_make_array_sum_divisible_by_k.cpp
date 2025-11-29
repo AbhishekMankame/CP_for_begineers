c1ce2e4a30658bf228988d2f0c4ad336580e5571
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-11-29 11:59:39 +0000

    leetcode 3512 question update

---
+++ leetcode/3512_minimum_operations_to_make_array_sum_divisible_by_k.cpp
@@ -0,0 +1,8 @@
+// Leetcode 3512 - Minimum Operations to Make Array Sum Divisible by K
+// Easy
+
+/*
+You are given an integer array nums and an integer k. You can perform the following operation any number of times:
+- Select an index i and replace nums[i] with nums[i]-1.
+Return the minimum number of operations required to make the sum of the array divisible by k.
+*/
