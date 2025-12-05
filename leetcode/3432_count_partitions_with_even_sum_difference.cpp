c4c8bd5bb037b26a59a2af6c7a704a501aa92beb
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-12-05 18:16:47 +0000

    code update

--- leetcode/3432_count_partitions_with_even_sum_difference.cpp
+++ leetcode/3432_count_partitions_with_even_sum_difference.cpp
@@ -34,4 +34,18 @@ All partitions result in an even sum difference.
 Constraints:
 - 2 <= n == nums.length <= 100
 - 1 <= nums[i] <= 100
+
+Topics: Array, Math, Prefix Sum
 */
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int countPartitions(vector<int>& nums){
+    int sum=0;
+    for(int i=0;i<nums.size();i++){
+        sum+=nums[i];
+    }
+    return sum%2==0?nums.size()-1:0;
+}
