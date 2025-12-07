8325c42d8bad2558ee44b37448a4c2d924182b15
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-12-07 19:26:19 +0000

    recursive approach code

--- basic-concepts/binary search/intro.cpp
+++ basic-concepts/binary search/intro.cpp
@@ -27,3 +27,14 @@ int binarySearch(const vector<int>& arr, int target){
     return -1;  // not found
 }
 
+
+// Recursive Approach
+int binarySearchRec(const vector<int>& arr, int left, int right, int target){
+    if(left>right) return -1;
+
+    int mid = left + (right - left)/2;
+
+    if(arr[mid]==target) return mid;
+    if(arr[mid]<target) return binarySearchRec(arr,mid+1,right,target);
+    else binarySearchRec(arr,left,mid-1,target);
+}
