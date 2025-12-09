e2557f51e638405e4daa680c82334b24b826ea80
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-12-09 18:07:14 +0000

    approach 2 code update

--- leetcode/3583_count_special_triplets.cpp
+++ leetcode/3583_count_special_triplets.cpp
@@ -75,3 +75,25 @@ int specialTripletsBruteForce(vector<int>& nums){
 
 // Time Complexity: O(n^3)
 // Space Complexity: O(1)
+
+
+/*Second approach*/
+
+int specialTriplets2(vector<int>&nums){
+    const int mod = 1e9+7;
+    int n=nums.size();
+    int ans=0;
+    for(int j=0;j<n;j++){
+        int Lcount=0;
+        for(int i=0;i<j;i++){
+            if(nums[i]==nums[j]*2) Lcount++;
+        }
+        int Rcount=0;
+        for(int k=j+1;k<n;k++){
+            if(nums[k]==nums[j]*2) Rcount++;
+        }
+        ans+=(long long)Lcount*Rcount;
+    }
+
+    return ans;
+}
