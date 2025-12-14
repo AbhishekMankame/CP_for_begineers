cd9e84e7ffe606a57b0cf1c66c67222f2184e11f
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-12-14 19:13:09 +0000

    code update

--- leetcode/34_find_first_and_last_position_of_element_in_sorted_array.cpp
+++ leetcode/34_find_first_and_last_position_of_element_in_sorted_array.cpp
@@ -27,3 +27,45 @@ Output:
 
 Topics: Array, Binary Search
 */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int firstPos(vector<int>& nums, int target){
+    int left=0,right=nums.size()-1,ans=-1;
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        if(nums[mid]==target){
+            ans=mid;
+            right=mid-1;
+        }
+        else if(nums[mid]>target) right=mid-1;
+        else left=mid+1;
+    }
+    return ans;
+}
+
+int lastPos(vector<int>& nums, int target){
+    int left=0,right=mid-1,ans=-1;
+    while(left<=right){
+        if(nums[mid]==target){
+            ans=mid;
+            left=mid+1;
+        }
+        else if(nums[mid]>target){
+            right=mid-1;
+        }
+        else left=mid+1;
+    }
+
+    return ans;
+}
+
+vector<int> searchRange(vector<int>& nums, int target{
+    int first=firstPos(nums,target);
+    int last=lastPos(nums,target);
+
+    return {first,last};
+}
