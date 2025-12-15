8331ba3eaf39340d5debbbe179e27a21d6737ada
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-12-15 17:54:26 +0000

    code update

--- leetcode/274_H-index.cpp
+++ leetcode/274_H-index.cpp
@@ -23,3 +23,29 @@ Constraints:
 Topics: Array, Sorting, Counting Sort
 
 */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int hIndex(vector<int>& citations){
+    sort(citations.begin(),citations.end(),greater<int>()); // Sort in descending order
+    int n=citations.size();
+    int left=0.right=n-1;
+    int ans=0;
+
+    // Binary search to find the H-index
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        if(citations[mid]>=mid+1) { // There are at least mid+1 papers with citations >= citations[mid]
+            ans=mid+1; // This could be a potential H-index
+            left=mid+1; // Try to find a higher H-index
+        }
+        else {
+            right=mid-1; // Otherwise, move the search range to the left
+        }
+    }
+
+    return ans;
+}
