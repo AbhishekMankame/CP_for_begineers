e30ca34a9a9e420f585dac2bb2e0f7f25fa33291
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-12-22 19:31:47 +0000

    qustion update

--- leetcode_discussion/google_sde3_round1_question.cpp
+++ leetcode_discussion/google_sde3_round1_question.cpp
@@ -7,4 +7,25 @@ You are given a set of N unique 2D point coordinates (x,y) on the cartesian plan
 
 A magical rectangle is an empty rectangular non-zero area parallel to the coordinate axes, delimited only by an upper left point and a lower right point, with no other points or within its area.
 
+Examples:
+Example 1:
+Input: [ (10,0), (0,10) ]
+Output: 1
+
+Example 2:
+Input: [ (0,0), (1,1), (2,2) ]
+Output:0
+
+Example 3:
+Input: [ (2, 0), (1, 1), (0, 2) ]
+Output: 2 (AB BC)
+
+Example 4:
+Input: [(0, 0), (1, 0)]
+Output: 0
+
+I was able to give only brute force approach which was N^3.
+
+Result - No hire
+
 */
