5ce3028caef8ced71914501619f4bc77f6c87851
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-12-22 19:30:23 +0000

    google swe 3 round 1 question update

---
+++ leetcode_discussion/google_sde3_round1_question.cpp
@@ -0,0 +1,10 @@
+// Google SWE 3 - Round 1 Question
+/*
+Found in leetcode discussion:
+Recently I gave the google interview. This was the question which was asked. Can anyone help me what can be the optimized approach for this?
+
+You are given a set of N unique 2D point coordinates (x,y) on the cartesian plane. Find the number of magical rectangles that can be formed with the given points.
+
+A magical rectangle is an empty rectangular non-zero area parallel to the coordinate axes, delimited only by an upper left point and a lower right point, with no other points or within its area.
+
+*/
