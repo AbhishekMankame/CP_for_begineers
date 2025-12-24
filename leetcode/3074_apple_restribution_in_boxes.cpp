161fbbb570075e64e2d17a0b154419daaf39837a
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-12-24 18:27:21 +0000

    leetcode 3074 questions update

---
+++ leetcode/3074_apple_restribution_in_boxes.cpp
@@ -0,0 +1,12 @@
+// Leetcode 3074 - Apple redistribution into boxes
+// Easy
+
+/*
+You are given an array apple of size n and an array capacity of size m.
+
+There are n packs where the ith pack contains apple[i] apples. There are m boxes as well, and the ith box has a capacity of capacity[i] apples.
+
+Return the minimum number of boxes you need to select to redestribute these n packs of apples into boxes.
+
+Note that, apples from the same pack can be redestributed into different boxes.
+*/
