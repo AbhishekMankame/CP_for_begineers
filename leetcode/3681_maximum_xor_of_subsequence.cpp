6723019b89374ec85da71ef270db57b82b578253
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2025-12-31 18:24:57 +0000

    leetcode 3186 question update

---
+++ leetcode/3681_maximum_xor_of_subsequence.cpp
@@ -0,0 +1,15 @@
+// Leetcode 3681 - Maximum XOR of Subsequence
+// Hard
+
+/*
+You are given an integer array 'nums' of length 'n' where each element is a non-negative integer.
+
+Select two subsequence of 'nums' (they may be empty and are allowed to overlap), each preserving the original order of elements, and let:
+- X be the bitwise XOR of all elements in the first subsequence.
+- Y be the bitwise XOR of all elements in the second subsequence.
+
+Return the maximum possible value of 'X XOR Y'.
+
+Note: The XOR of an empty subsequence is 0.
+
+*/
