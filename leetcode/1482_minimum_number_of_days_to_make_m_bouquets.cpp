77f6d39e0604b48c181e50438a40e54b2d8d836c
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-01 11:07:37 +0000

    possible function code

--- leetcode/1482_minimum_number_of_days_to_make_m_bouquets.cpp
+++ leetcode/1482_minimum_number_of_days_to_make_m_bouquets.cpp
@@ -42,3 +42,20 @@ Constraints:
 
 Topics: Array, Binary Search
 */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool possible(vector<int>& bloomDay, int day, int m, int k){
+    int count=0,no=0;
+    for(int i=0;i<bloomDay.size();i++){
+        if(bloomDay[i]<=day) count++;
+        else {
+            no+=count/k;
+            count=0;
+        }
+        return no>=m?true:false;
+    }
+}
