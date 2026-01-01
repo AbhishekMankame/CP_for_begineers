0182753ee3868e1e166bd3e52979bc2c974e09c6
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-01 11:11:12 +0000

    code update

--- leetcode/1482_minimum_number_of_days_to_make_m_bouquets.cpp
+++ leetcode/1482_minimum_number_of_days_to_make_m_bouquets.cpp
@@ -59,3 +59,21 @@ bool possible(vector<int>& bloomDay, int day, int m, int k){
         return no>=m?true:false;
     }
 }
+
+int minDays(vector<int>& bloomDay, long long int m, long long int k){
+    int n=bloomDay.size();
+    if(m*k>n) return -1;
+    int low = *min_element(bloomDay.begin(),bloomDay.end());
+    int high = *max_element(bloomDay.begin(),bloomDay.end());
+
+    while(low<=high){
+        int mid = low + (high-low)/2;
+        if(possible(bloomDay,mid,m,k)){
+            ans=mid;
+            high=mid-1;
+        }
+        else low=mid+1;
+    }
+
+    return ans;
+}
