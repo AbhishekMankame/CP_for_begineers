29388994767f3de21f2eff7727e416beb727ac00
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-02 18:49:54 +0000

    complexity update

--- leetcode/961_n-repeated_elements_in_size_2N_array.cpp
+++ leetcode/961_n-repeated_elements_in_size_2N_array.cpp
@@ -30,3 +30,18 @@ Constraints:
 Topics: Array, Hash Table
 
 */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int repeatedNTimes(vector<int>&nums){
+    for(int i=0;i<nums.size()-2;i++){
+        if(nums[i]==nums[i+1] || nums[i]==nums[i+2]) return nums[i];
+    }
+    return nums.back();
+}
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)
