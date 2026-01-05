6c2d7babb2c2dd884e1712bae075b567f5564855
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-05 07:12:19 +0000

    explanation update

--- grinding/subarray/introduction.cpp
+++ grinding/subarray/introduction.cpp
@@ -73,3 +73,63 @@ int maxSubarraySum(vector<int> &arr, int k){
 }
 
 // Brain Shift: "I am not recalculating the sum, I am updating it."
+
+/*
+Let's go through the above code indetail (step by step explanation)
+1. Variables
+int windowSum=0;
+int maxSum=0;
+
+- windowSum --> sum of the current window
+- maxSum --> best (maximum) window sum seen so far
+
+Think: "windowSum always represents the sum of elements inside the current window."
+
+2. The loop
+for(int i=0;i<arr.size();i++)
+- i is the right end 
+At each step: we expand the window to the right
+
+3. Add current element
+window += arr[i]
+You are saying: "include arr[i] in my window."
+Now the window temorarily grows.
+
+4. When does the window become valid?
+if(i>=k-1)
+- This is VERY IMPORTANT
+
+why k-1?
+- window size = k
+- Find valid window ends at index k-1
+
+Example:
+k=3
+indicies: 0 1 2
+
+So
+- Before i=k-1, window size < k --> ignore
+- Fromi=k-1 onward --> window size = k
+
+5. Update the answer
+maxSum = max(maxSum, windowSum);
+
+Now the window size is exactly k:
+- compare current window sum
+- store the maximum
+
+6. Slide the window forward
+window -= arr[i - (k-1)];
+This line is the magic
+
+what is i-(k-1)?
+
+current window:
+end = i
+size = k
+start = i-k+1
+
+so start = i - (k-1)
+We are removing left most element of the window.
+Think: "I've used the window, now remove the oldest element and move forward."
+*/
