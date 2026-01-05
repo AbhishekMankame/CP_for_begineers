735ea0a3a812a35334f94d0e0e637ef1a012cbe9
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-05 05:18:42 +0000

    generate all subarray brute force approach update

--- grinding/subarray/introduction.cpp
+++ grinding/subarray/introduction.cpp
@@ -13,3 +13,34 @@ If it breaks continuously, then it is NOT a subarray.
 
 Very very important (Golden Rule): If it's a subarray problem, indices are continuous --> think sliding window or prefix sum
 */
+
+/*
+Patterns 1: Generate all subarrays (Brute Force)
+This is where everyone must start
+
+Thinking:
+- Fix a start
+- Extend the end
+- Every (start, end) pair is a subarray
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr = {1,2,3};
+    int n=arr.size();
+
+    for(int start=0;start<n;start++){
+        for(int end=start;end<n;end++){
+            cout << arr[i] << " ";
+        }
+        cout<<endl;
+    }
+}
+
+/*
+Important:
+- This is not useless
+- Every optimized solution starts frrom THIS thinking
+*/
