d28874fc6cb9071146ea068589141344bb7a0592
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-05 05:12:05 +0000

    grinding subarray introduction update

---
+++ grinding/subarray/introduction.cpp
@@ -0,0 +1,15 @@
+// Subarray
+
+/*
+Note: Hard problems feel easy only after patterns become automatic.
+So my goal is to make subarray patterns automatic in my brain.
+
+What is subarray?
+- A subarray is a continuous part of an array.
+
+Eg: Array = [1,2,3]
+Subarrays: [1], [2], [3], [1,2], [2,3], [1,2,3]
+If it breaks continuously, then it is NOT a subarray.
+
+Very very important (Golden Rule): If it's a subarray problem, indices are continuous --> think sliding window or prefix sum
+*/
