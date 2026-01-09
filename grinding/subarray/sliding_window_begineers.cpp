abe5c7de85118d63a5428d1de9377820974942da
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-09 20:48:10 +0000

    sliding window core idea

--- grinding/subarray/sliding_window_begineers.cpp
+++ grinding/subarray/sliding_window_begineers.cpp
@@ -3,7 +3,7 @@ Sliding Window
 It is an algorithmic technique used to efficiently process contiguous subarrays or substrings by maintaining a window of elements that moves forward step by step, instead of recomputing results from scratch for each position.
 It reduces time complexity from O(n^2) to O(n) in many problems.
 
-When to use Sliding Window
+### When to use Sliding Window
 Use sliding window when:
 - The problem involves subarray/substring
 - Elements must be contiguous
@@ -13,5 +13,12 @@ Use sliding window when:
     - length/count
     - constraints like at most k, exactly k, no repeating
 Do not use sliding window if elements are not contiguous
+
+### Core Idea (Intution)
+Instead of recalculating values for every position:
+- Add the new element entering the window (right side)
+- Remove the element leaving the window (left side)
+- Update the answer using the current window
+This reuse of previous computation makes it efficient.
 
 */
