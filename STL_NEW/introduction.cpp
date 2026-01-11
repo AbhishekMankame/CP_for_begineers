7992728c4a93c53f591ee2f35317d97df0fa7ac4
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-11 07:40:06 +0000

    C++ STL introduction update

---
+++ STL_NEW/introduction.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h> // All the libraries are present inside stdc++.h, so all the libraries are automatically included
+// so we need not to include each library individually
+using namespace std; // If we do not write this line, then everytime we need to use std:: for cin and cout
+
+int main(){
+
+    int a;
+    cin>>a;
+    cout<<a;
+
+
+    return 0;
+}
