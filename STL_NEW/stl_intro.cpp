363374450b9c1278d201a36cb6eeaaa37c9b36bb
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-11 08:07:46 +0000

    C++ STL and pair introduction

---
+++ STL_NEW/stl_intro.cpp
@@ -0,0 +1,15 @@
+/*
+C++ STL is divided into 4 parts
+Algorithms
+Containers
+Functions
+Iterators
+*/
+
+/*
+Pairs: In C++, a pair standard library container that stores two values together as a single unit.
+The two values can be of same or different datatypes.
+It is defined in the <utility> header and provided by the STL (Standard Template Library)
+
+Syntax: pair<dataType1, dataType2> pairName;
+*/
