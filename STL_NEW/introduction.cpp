d2ae55d1487e584b9192d4b788f6a0d07281d027
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-11 07:44:28 +0000

    C++ functions update

--- STL_NEW/introduction.cpp
+++ STL_NEW/introduction.cpp
@@ -2,12 +2,25 @@
 // so we need not to include each library individually
 using namespace std; // If we do not write this line, then everytime we need to use std:: for cin and cout
 
+void print(){ // void function does not returns anything
+    cout<<"raj";
+}
+
+
+// Return type function
+int sum(int a, int b){ // This function return the integer type
+    return a+b;
+}
+
+// Note: we can use any datatypes as return type
+
 int main(){
 
     int a;
     cin>>a;
     cout<<a;
-
-
+    print(); // This print function calls the above print function and prints "raj" in the screen
+    int s = sum(3,5); // This will return the sum of 3 and 5
+    cout << s; // Returns 8
     return 0;
 }
