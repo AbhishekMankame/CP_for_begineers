cefcfe08f029607f96a80bc35d3b63186be8990c
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-12 18:32:00 +0000

    code update

--- leetcode/75_sorted_array.cpp
+++ leetcode/75_sorted_array.cpp
@@ -23,3 +23,18 @@ Follow up: Could you come up with a one-pass algorithm using only constant extra
 
 Topics: Array, Two Pointers, Sorting
 */
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target){
+    int i=0,j=matrix[0].size()-1;
+    while(i<matrix.size()&&j>=0){
+        if(matrix[i][j]==target) return true;
+        else if(matrix[i][j]>target) i--;
+        else j++;
+    }
+    return false;
+}
