48a344868f7c3051e4be71ff37386f47add971c0
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-14 07:58:35 +0000

    C++ queue update

--- STL_NEW/list_intro.cpp
+++ STL_NEW/list_intro.cpp
@@ -56,3 +56,22 @@ void exploreStack() {
 
     // Complexity: In stack, push, pop and top operations all will take O(1) operations (constant time)
 }
+
+void exploreQueue(){
+    queue<it> q;
+    q.push(1); // {1}
+    q.push(2); // {1, 2}
+    q.emplace(4); // {1, 2, 4}
+
+    q.back()+5;
+    cout << q.back(); // prints 9
+
+    // Q is {1, 2, 9}
+    cout << q.front(); // prints 1
+
+    q.pop(); // {2, 9}
+
+    cout << q.front(); // prints 2
+
+    // size, swap, empty is same as stack
+}
