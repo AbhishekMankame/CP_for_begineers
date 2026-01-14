044f523f645c95ffcf9c1d2b6d716a55f342ba3d
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-14 07:41:15 +0000

    C++ deque update

--- STL_NEW/list_intro.cpp
+++ STL_NEW/list_intro.cpp
@@ -14,3 +14,20 @@ void exploreList(){
     // rest functions same as vector 
     // begin, end, rbegin, rend, clear, insert, size, swap
 }
+
+void exploreDeque(){
+    deque<int> dq;
+    dq.push_back(1); // {1}
+    dq.emplace_back(2); // {1, 2}
+    dq.push_front(4); // {4, 1, 2}
+    dq.emplace_front(3); // {3, 4, 1, 2}
+
+    dq.pop_back(); // {3, 4, 1}
+    dq.pop_front(); // {4, 1}
+
+    dq.back(); // Last element of deque
+    dq.front(); // First element of deque
+
+    // rest functions same as vector
+    // begin, end, rbegin, rend, clear, insert, size, swap
+}
