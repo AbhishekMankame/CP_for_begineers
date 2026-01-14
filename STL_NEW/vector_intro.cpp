0ca1d3e98a7315d44a9443252123f50bb0a4f219
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-14 07:06:02 +0000

    insert and copy of vector

--- STL_NEW/vector_intro.cpp
+++ STL_NEW/vector_intro.cpp
@@ -9,5 +9,15 @@ void vector(vector<int> v){
 
     // If we want to delete the multiple elements
     // {10,20,12,23,35}
-    v.erase(v.begin()+2, v.begin()+4); // {10,20,35} --> Here it will follow [start,end)
+    v.erase(v.begin()+2, v.begin()+4); // {10,20,35} --> Here it will follow [start,end), here the end address is after the element
+
+    // Insert function
+
+    vector<int> v(2,100) // {100, 100}
+    v.insert(v.begin(),300); // {300, 100, 100}
+    v.insert(v.begin()+1,2,10); // {300, 10, 10, 100, 100}
+
+    // copying vector
+    vector<int> copy(2,50); // {50, 50}
+    v.insert(v.begin(), copy.begin(),copy.end()); // {50, 50, 300, 10, 10, 100, 100}
 }   
