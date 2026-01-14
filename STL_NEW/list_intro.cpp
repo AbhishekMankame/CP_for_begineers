709d5e0fc524f7031740554607a0ddaa774033ce
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-14 08:41:06 +0000

    C++ Priority Queue update

--- STL_NEW/list_intro.cpp
+++ STL_NEW/list_intro.cpp
@@ -58,6 +58,7 @@ void exploreStack() {
 }
 
 void exploreQueue(){
+    // Queue is FIFO --> First In First Out --> The element which goes in First comes out First
     queue<it> q;
     q.push(1); // {1}
     q.push(2); // {1, 2}
@@ -74,4 +75,30 @@ void exploreQueue(){
     cout << q.front(); // prints 2
 
     // size, swap, empty is same as stack
+}
+
+void explain PQ() {
+    priority_queue<int> pq;
+
+    pq.push(5); // {5}
+    pq.push(2); // {5, 2}
+    pq.push(8); // {8, 5, 2}
+    pq.emplace(10); // {10, 8, 5, 2}
+    
+    cout << pq.top(); // prints 10
+    
+    pq.pop(); // {8, 5, 2}
+
+    cout << pq.top(); // prints 8
+
+    // size, swap, empty function same as others
+
+    // Minimum Heap
+    priority_queue<int, vector<int>, greater<int>> pq;
+    pq.push(5); // {5}
+    pq.push(2); // {2, 5}
+    pq.push(8); // {2, 5, 8}
+    pq.emplace(10); // {2, 5, 8, 10}
+
+    cout << pq.top(); // prints 2
 }
