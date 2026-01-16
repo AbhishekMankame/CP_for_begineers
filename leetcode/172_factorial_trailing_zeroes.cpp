c3a7a82546bdbab6f4155b561270b04d08f92e57
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-16 06:28:29 +0000

    approach update

--- leetcode/172_factorial_trailing_zeroes.cpp
+++ leetcode/172_factorial_trailing_zeroes.cpp
@@ -26,3 +26,19 @@ Follow up: Could you write a solution that works in logarithmic time complexity?
 Topic: Math
 
 */
+
+/*
+Key idea: Trailing zeros in a factorial come from the factors of 10: 10 = 2 * 5
+In 'n!', there are far more 2s than 5s, so the number of trailing zeros depends entirely on how many time 5 appears as a factor.
+
+How to count factors of 5?
+- Every multiple of 5 contributes atleast one 5
+- Every multiple of 25 contributes an extra 5
+- Every multiple of 125 contributes an another 5
+- And so on...
+
+So the total number of trailing zeros is:
+floor(n/5) + floor(n/25) + floor(n/125) + ...
+Stop when the divisor exceeds 'n'.
+
+*/
