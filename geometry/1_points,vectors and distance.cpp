3437486c063a94a0c50d8757c2640a3104137d56
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-18 08:02:22 +0000

    CP geometry basics intro

---
+++ geometry/1_points,vectors and distance.cpp
@@ -0,0 +1,12 @@
+// Lesson 1 - Points, vectors and distance
+
+/*
+1.1 Cartesion Coordinate System
+
+In CP geometry, all objects are placed on a 2-dimensional Cartesion plane.
+Each position on this plane is represented by a point, which is defined by two real numbers: an x-coordinate and a y-coordinate.
+The x-coordinate represents horizontal movement (left or right), while the y-coordinate represents vertical movement (up or down).
+
+For example, the point P(2,3) lies in 2 units to the right of the origin and 3 units above it.
+
+*/
