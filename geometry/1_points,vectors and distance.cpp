c32378254b401bb732ccbeac30042cfeda0e0c0d
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-18 08:30:33 +0000

    distance between two points

--- geometry/1_points,vectors and distance.cpp
+++ geometry/1_points,vectors and distance.cpp
@@ -9,4 +9,32 @@ The x-coordinate represents horizontal movement (left or right), while the y-coo
 
 For example, the point P(2,3) lies in 2 units to the right of the origin and 3 units above it.
 
+1.2 Representing a Point in Code
+A point in geometry contains only two pieces of information: its x and y coordinates.
+Therefore, in C++, a point is best represented using a 'struct' or 'class' with two floating-point variables.
+
+We usually use 'double' for coordinates because it provides enough precision for most problems. Later, when precision becomes critical, we may use 'long double'.
 */
+
+struct Point {
+    double x,y;
+};
+
+/*
+1.3 Distance Between Two Points
+The distance between two points is derived from the Pythagorean theorem.
+If we have two points A(x1,y1) and B(x2,y2), the horizontal difference between them is x2-x1, and the vertical distance is y2-y1.
+
+These differences form the legs of a right-angled triangle. The distance between the points is the length of the hypotenuse:
+
+distance = sqrt((x2-x1)^2+(y2-y1)^2)
+
+This formula is used constantly in geometry problems.
+
+*/
+
+double distance(Point a, Point b){
+    double dx = a.x - b.x;
+    double dy = a.y - b.y;
+    return sqrt(dx*dx+dy*dy);
+}
