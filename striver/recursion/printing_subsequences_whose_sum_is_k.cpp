310fac824d7622f083daa5ff2415be90f1f6e3ff
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-24 07:15:31 +0000

    code update

--- striver/recursion/printing_subsequences_whose_sum_is_k.cpp
+++ striver/recursion/printing_subsequences_whose_sum_is_k.cpp
@@ -5,6 +5,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void printS(int ind, vector<int> &ds, int s, int sum, int arr[], int n){
+    if(ind==n){
+        if(s==sum){
+            for(auto it:ds) cout<<it << " ";
+            cout<<endl;
+        }
+        return;
+    }
+    ds.push_back(arr[ind]);
+    s += arr[ind];
+    printS(ind+1,ds,s,sum,arr,n);
+    s -= arr[ind];
+    ds.pop_back();
+    // Not pick
+    printS(ind+1,ds,s,sum,arr,n);
+}
+
 int main() {
     int arr = {1, 2, 1};
     int n = 3, sum = 2;
