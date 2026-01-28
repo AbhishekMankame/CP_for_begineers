3af60d3407e9863678e4fd30f714a72a2bda11fb
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-28 19:08:23 +0000

    modular multiplication and GCD update

--- CP/Math for CP/modulo.cpp
+++ CP/Math for CP/modulo.cpp
@@ -13,4 +13,19 @@ Modular Addition: Modular Addition has the following formula:
 Modular Subtraction: Modular Subtraction has the following formula:
 (A-B)%M = ((A%M-B%M)+M)%M
 Note: Careful with the extra M outside
+
+Modular Multiplication: Modular Multiplication has the following formula:
+(A*B)%M = ((A%M)*(B%M))%M
+Note: Since the operator precedence level of "%","/","*" are all same, they are executed from the left to right in a single line. Hence, the inner brackets are must for correct execution of Modular Multiplication formula.
+
+*/
+
+/*
+Greatest Common Divisor(GCD) (also called as HCF - Highest Common Factor)
+Greatest Common Divisor of two or more integers, which are not all zero, is the largest positive integer that divides each of the integers.
+Example: The GCD of 8 and 12 are 4, that is, GCD(8,12) = 4
+
+Euclidean Algorithm: If we subtract a smaller number from a larger one (we reduce a larger number), GCD doesn't change. So if we keep subtracting repeatedly the larger of two, we end up with GCD.
+GCD(A,B) = GCD(A-B,B), assuming A>=B
+- When difference reaches 0, the solution is B.
 */
