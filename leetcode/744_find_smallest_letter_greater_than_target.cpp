f675ef8094b0e43f4df380b42c6216f608eff256
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-01-31 19:22:45 +0000

    code update

--- leetcode/744_find_smallest_letter_greater_than_target.cpp
+++ leetcode/744_find_smallest_letter_greater_than_target.cpp
@@ -32,3 +32,20 @@ Constraints:
 Topics: Array, Binary Search
 
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+char mextGreatestLetter(vector<int>& letters, char target){
+    int low = 0, high = letters.size()-1,ans=0;
+    while(low<=high){
+        int mid = low + (high-low)/2;
+        if(letters[mid]>target){
+            ans=mid;
+            high=mid-1;
+        }
+        else low=mid+1;
+    }
+
+    return letters[ans];
+}
