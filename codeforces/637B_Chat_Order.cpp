5330bad7a3ada49f4ca5c408013aaaf5b082d8f9
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-02-04 21:05:09 +0000

    codeforces 637B question update

---
+++ codeforces/637B_Chat_Order.cpp
@@ -0,0 +1,34 @@
+// https://codeforces.com/problemset/problem/637/B
+
+// 637B - Chat Order
+// time limit per test: 3 seconds
+// memory limit per test: 256 megabytes
+
+/*
+Polycarp is a big lover of killing time in social networks. A page with a chatlist in his favourite network is made so that when a message is sent to some friend, his friend's chat rises to the very top of the page.
+The relative order of the other chats doesn't change. If there was no chat with this friend before, then a new chat is simply inserted to the top of the list.
+
+Assuming that the chats list is initially empty, given the sequence of Polycarpus' messages make a list of chats after all of his messages are processed. Assume that no friend wrote any message to Polycarpus.
+
+Input:
+The first line contains integer n (1<=n<=200000) - the number of Polycarpus' messages. Next n lines enlist the message recipients in the order in which the messages were sent. The name of each participant is a non-empty sequence of lowercase Englist letters of length at most 10.
+
+Output:
+Print all the recipients to who Polycarp talked to in the order of chats with them, from top to bottom.
+
+Examples:
+Input:
+4
+alex
+ivan
+roman
+ivan
+
+Output:
+ivan
+roman
+alex
+
+*/
+
+// Problem tags: *special problem, binary search, constructive algorithm, data structure, sortings, *1200
