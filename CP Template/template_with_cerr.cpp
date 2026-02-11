b690882006fb33be421c7dbec74a74772a7a9430
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-02-11 06:46:17 +0000

    get template define update

---
+++ CP Template/template_with_cerr.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>
+#define debug(x) _print(x); cerr<<endl;
+void _print(ll t) {cerr<<t;}
+void _print(int t) {cerr<<t;}
+void _print(string t) {cerr<<t;}
+void _print(char t) {cerr<<t;}
+void _print(double t) {cerr<<t;}
