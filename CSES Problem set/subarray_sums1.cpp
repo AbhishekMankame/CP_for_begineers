0ce52e431e88e6b54379d34c8560d4cbd7726907
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-02-19 18:56:30 +0000

    code update

--- CSES Problem set/subarray_sums1.cpp
+++ CSES Problem set/subarray_sums1.cpp
@@ -21,3 +21,35 @@ Input:
 Output:
 3
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    long long x;
+    cin>>n>>x;
+
+    vector<long long> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    long long sum=0;
+    long long count=0;
+    int left=0;
+
+    for(int right=0;right<n;right++){
+        sum+=arr[right];
+        while(sum>x){
+            sum-=arr[left];
+            left++;
+        }
+        if(sum==x) count++;
+    }
+    cout<<count<<endl;
+    return 0;
+}
