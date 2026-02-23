361ef53b8e9b680db8a5c222bafee10484e7e7e4
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-02-23 14:26:26 +0000

    code update

--- leetcode/codeforces_round_849(div-4)/A_codeforces_checking.cpp
+++ leetcode/codeforces_round_849(div-4)/A_codeforces_checking.cpp
@@ -40,3 +40,21 @@ NO
 Problem tags: implementation, strings, *800
 
 */
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin>>t;
+    while(t--){
+        char c;
+        cin>>c;
+        if(c=='c' || c=='o' || c=='d' || c=='e' || c=='f' || c=='r' || c=='s') cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+    return 0;
+}
