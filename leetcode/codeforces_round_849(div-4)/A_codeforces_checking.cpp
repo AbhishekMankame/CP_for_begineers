15ddff6421bff7bfedfa0abdddb5fa0843b80798
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-02-23 14:23:13 +0000

    codeforces round 849 question update

---
+++ leetcode/codeforces_round_849(div-4)/A_codeforces_checking.cpp
@@ -0,0 +1,42 @@
+// A. Codeforces Checking
+// https://codeforces.com/contest/1791/problem/A
+
+/*
+Given a lowercase Latin character (letter), check if it appears in the string 'codeforces'.
+
+Input:
+The first line of the input contains an integer 't' (1<=t<=26) - the number of test cases.
+The only line of each test case contains a character 'c' - a single lowercase Latin character (letter).
+
+Output:
+For each test case, output "YES" (without quotes) if 'c' satisfies the condition, and "NO" (without quotes) otherwise.
+You can output the answer in any case (for example, the string "yEs", "yes", "Yes" and "YES" will be recognized as a positive answer).
+
+input:
+10
+a
+b
+c
+d
+e
+f
+g
+h
+i
+j
+
+output:
+NO
+NO
+YES
+YES
+YES
+YES
+NO
+NO
+NO
+NO
+
+Problem tags: implementation, strings, *800
+
+*/
