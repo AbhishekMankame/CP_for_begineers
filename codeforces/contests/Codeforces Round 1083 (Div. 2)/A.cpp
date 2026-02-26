1922bb5c41e98da52ef247cd2a88df3e71d8a2b9
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-02-26 18:04:05 +0000

    codeforces round 1083 A question intution update

---
+++ codeforces/contests/Codeforces Round 1083 (Div. 2)/A.cpp
@@ -0,0 +1,32 @@
+// A. Simons and Making it beautiful
+// https://codeforces.com/contest/2205/problem/A
+
+/*
+Intution First: i=max(p1,p2,..,pi)
+That means: The maximum value seen so far in the prefix is exactly equal to the position.
+
+The maximum number of ugly indices happens when the permutation is: [1,2,3,4,...,n]
+Every index is ugly.
+So ugly indices happen when the permutation behaves "too sorted" in prefix.
+To reduce ugly indices, we want to:
+- Break prefix structure
+- Delay small numbers from forming clean prefixes
+
+## Powerful insight
+If we swap the first element and the largest element(n):
+- We immediately break all early prefixes.
+- Because now the first prefix is already contains 'n', so the max becomes 'n' very early.
+- So for all i<n, max(prefix)!=i.
+This kills almost all ugly indices.
+
+## Best stratergy:
+For each test case:
+1. If n=1: Just print it (only one possible answer).
+2. Otherwise:
+    - Find position of 'n'
+    - Swap p[1] and p[pos_of_n]
+That's it...
+This guarantees:
+    - Atmost one ugly index remains (usually at position n)
+    - And that's optimal
+*/
