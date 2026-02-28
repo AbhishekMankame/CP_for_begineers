5a7f8419e6c42cc45bf799066fea04461fe43c0e
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-02-28 06:24:21 +0000

    second largest element optimal approach

--- striver/arrays/pratice_problems.cpp
+++ striver/arrays/pratice_problems.cpp
@@ -12,7 +12,7 @@ int largestElement(int arr, int n){
     for(int i=1;i<n;i++){
         if(a[i]>ans) ans=a[i];
     }
-    cout<<ans<<endl;
+    return ans;
 }
 
 // TC: O(n)
@@ -41,6 +41,7 @@ int secondLargestBrute(vector<int>& arr){
             break;
         }
     }
+    return second_largest;
 }
 
 // TC: O(b log n + n) == O(n log n)
@@ -64,8 +65,42 @@ int secondLargestBetter(vector<int> &arr){
     for(int i=0;i<n;i++){
         if(arr[i]>second_largest && arr[i]!=largest) second_largest=arr[i];
     }
-    cout<<second_largest<<endl;
+    return second_largest;
 }
 
 // TC: O(N) + O(N) == O(2N) ==> O(N)
 // SC: O(1)
+
+
+/*
+Optimal approach:
+- We will consider first element as the largest => largest = arr[0]
+- Second largest we will consider as INT_MIN ==> second_largest = INT_MIN
+- Now we will check at first index (second element), if it is greater than largest, then that element will be the largest and the previous largest will become second_largest
+- Note: If any element is equivalent to largest, then do not do anything
+
+*/
+
+int secondLargestOptimal(vector<int> &arr){
+    int n=arr.size();
+    int largest=arr[0],second_largest=INT_MIN;
+    for(int i=1;i<n;i++){
+        if(arr[i]>largest) {
+            second_largest=largest;
+            largest=arr[i];
+        }
+        else if(arr[i]>second_largest && arr[i]<largest){
+            second_largest=arr[i];
+        }
+    }
+    return second_largest;
+}
+
+/*
+Complexity details:
+Here in optimal solution to find the second largest just takes one pass => O(N)
+TC: O(N)
+SC: O(1)
+
+*/
+
