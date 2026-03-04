7757764bea35f5a99f921fde44314ff7650a30eb
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-03-04 06:15:59 +0000

    linked list intro

---
+++ linked list/introduction.cpp
@@ -0,0 +1,16 @@
+// Linked List
+
+/* 
+What is a Linked List?
+Imagine you are given a task where you have to maintain a data entry of cars entering a parking lot.
+Since the number of cars entering will be different and may change daily, constructing a fixed-sized data structure like an array might not be helpful.
+This is when linked lists come into the picture, which allows us to add and remove cars easily.
+Linked List is a linear data structure that can be visualized as a chain with different nodes connected, where each node represents a different elements.
+The difference between arrays and linked list is that, unlike arrays, the elements are not stored at contiguous location.
+
+For any elements to be added in an array, we need the exact next memory location to be empty and it is impossible to guarantee that it is possible. Hence adding elements to an array is not possible after the initial assignment of size.
+
+A linked list is a data structure containing two crucial pieces of information, the first being the data and the other being the pointer to the next element.
+The 'head' is the first node, and the 'tail' is the last node in a linked list.
+
+*/
