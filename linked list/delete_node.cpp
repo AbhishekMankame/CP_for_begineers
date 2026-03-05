dc77e85c49458adbd109d9ba6f198f36c7bf4312
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-03-05 15:44:01 +0000

    remove node with given element

--- linked list/delete_node.cpp
+++ linked list/delete_node.cpp
@@ -81,6 +81,30 @@ Node* removeK(Node* head,int k){
     return head;
 
 }
+// TC: O(k)
+
+Node* removeEl(Node* head,int el){
+    if(head==NULL) return head;
+    if(head->data==el) {
+        Node* temp = head;
+        head=head->next;
+        delete temp;
+        return head;
+    }
+    Node* temp=head;
+    Node* prev=NULL;
+    while(temp!=NULL){
+        if(temp->data==el){
+            prev->next=prev->next->next;
+            free(temp);
+            break;
+        }
+        prev=temp;
+        temp=temp->next;
+    }
+    return head;
+
+}
 
 int main(){
     vector<int> arr = {12,5,8,7};
@@ -88,5 +112,6 @@ int main(){
     head = removesHead(head); 
     head = removesTail(head);
     head = removeK(head,3);
+    head = removeEl(head,3);
     print(head);
 }
