c139001563639d4072f6ff9eab90a1e578ba7074
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-03-05 01:26:16 +0000

    array to linked list update

---
+++ linked list/ll.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    public:
+    Node(int data1, Node* next1){
+        data=data1;
+        next=next1;
+    }
+
+    public:
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+    }
+};
+
+Node* convertArr2LL(vector<int> &arr){ // Node* --> Pointer to the head, we will be returning
+    Node* head = new Node(arr[0]); // As we have multi constructor, need not to pass nullptr
+    Node* mover = head;
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover->next = temp;
+        // We need to know that mover has to go front (or to the next)
+        mover=temp; // or mover=mover.next
+    }
+    return head;
+}
+
+int main() {
+    vector<int> arr = {2,5,8,7};
+    Node* head = convertArr2LL(arr);
+    cout<<head->data;
+}
