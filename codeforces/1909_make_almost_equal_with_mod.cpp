28e6318b7703867a1892ab18d5554b7413b32be0
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-03-08 11:45:13 +0000

    optimized code approach

--- codeforces/1909_make_almost_equal_with_mod.cpp
+++ codeforces/1909_make_almost_equal_with_mod.cpp
@@ -28,4 +28,28 @@ That means: array must contain both even and odd
 If all numbers are even: [2,4,6] then a[i]%2 -> 0, then in this case only 1 remainder, which is not valid.
 Same with array consisting of all odd numbers also give 1, that is also not valid.
 
+We will use power of 2 method, basically we will do modulo with 2^k, means we will start from 2, then 4, then 8, 16, ...
+we reveal mode bits, eventually we reach a bit where numbers differ.
+At the moment we get two different remainders.
+
 */
+
+// Short version code logic
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<long long> a(n);
+        for(auto &x:a) cin>>x;
+        long long x = 0;
+        for(int i=1;i<n;i++) x|=(a[i]^a[0]);
+        cout<<2*(x&-x)<<"\n";
+    }
+}
+
+// TC: O(t*n)
