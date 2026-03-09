434d7a10b3d4ab7cec37af4e4e16b09dbb785765
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-03-09 18:22:12 +0000

    contest 1742 A update

---
+++ codeforces/contests/contest1742-round827-div4/a_sum.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/contest/1742/problem/A
+
+// A - Sum
+
+#include<iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c;
+        cin>>a>>b>>c;
+        if((a+b==c) || (a+c==b) || (b+c==a)) cout<<"YES\n";
+        else cout<<"NO\n";
+    }
+    return 0;
+}
