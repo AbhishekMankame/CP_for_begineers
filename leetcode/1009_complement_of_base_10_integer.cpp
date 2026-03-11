46f2c38f078e3de87c42c8033ec8387779deb28c
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-03-11 14:13:52 +0000

    complexity details

--- leetcode/1009_complement_of_base_10_integer.cpp
+++ leetcode/1009_complement_of_base_10_integer.cpp
@@ -18,3 +18,35 @@ public:
         return mask^n;
     }
 };
+
+/*
+Code Logic:
+The algorithm:
+1. Handles the special case 'n==0'
+2. Creates a bitmask of all 1s with the same bit-length as 'n'
+3. Uses XOR between the mask and 'n' to flip the bits.
+
+Example:
+n = 5 (101 (base 2))
+mask --> 111 (base 2)
+101^101 = 010 --> 2
+
+Time Complexity:
+The main work happens in this loop:
+while(temp>0){
+    mask=(mask<<1)|1;
+    temp>>=1;
+}
+
+Each iterations shifts 'temp' right by 1 bit: temp >> 1
+So the loop runs once per bit of 'n'.
+If 'n' has k bits, then k=floor(log(2))+1
+Therefore: Time Complexity=O(k)
+where k=number of bits in 'n'
+Since k=log(n)
+
+Final time complexity: O(log n)
+
+Space complexity: O(1) --> No additional data structures or recursion is used.
+
+*/
