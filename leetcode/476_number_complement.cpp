8c547c2bc4e93f7db4da63507375976a7ad53262
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-03-13 17:46:30 +0000

    leetcode 476 update

---
+++ leetcode/476_number_complement.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/number-complement/
+
+// 476 - Number Complement
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+    int findComplement(int n){
+        int temp=n;
+        int mask=0;
+        while(temp){
+            mask = (mask<<1)|1;
+            temp>>=1;
+        }
+        return mask^n;
+    }
+}
