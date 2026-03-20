8e54668cd3f79f55b365fb1e541123266ba17cda
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-03-20 11:01:06 +0000

    monotonic stack example update

---
+++ concepts/stacks/monotonic_stack_example.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    vector<int> ans(n);
+    stack<int> s;
+
+    // NSL -> Next Smaller Element on Left side
+    for(int i=0;i<n;i++){
+        // rempving all the useless elements
+        while(!s.empty()&&s.top()>=v[i]){
+            s.pop(); // popping the element from the stack
+        }
+        if(s.empty()){
+            ans[i]=-1;
+        }
+        else{
+            ans[i]=s.top();
+        }
+        s.push(v[i]);
+    }
+    for(int i=0;i<n;i++){
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt","w",stderr);
+#endif
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
