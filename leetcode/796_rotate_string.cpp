198faed203b9b53187b5a5977b9f98329378a031
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-03-23 18:28:02 +0000

    leetcode 796 code update

--- leetcode/796_rotate_string.cpp
+++ leetcode/796_rotate_string.cpp
@@ -1,2 +1,12 @@
 // Leetcode 796 - Rotate String
 // https://leetcode.com/problems/rotate-string/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool rotateString(string s, string goal){
+    if(s.size()!=goal.size()) return false;
+    s+=s;
+    if(s.find(goal)!=string::npos) return false;
+    else return true;
+}
