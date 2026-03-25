8957c5863f48d733024d96d5f5f69d115475d4b2
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-03-25 14:29:47 +0000

    heapify down code

---
+++ data-structures/heaps/heap__operations2.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Max_Heap {
+    vector<int> heap;
+    
+    void heapify_down(int ind){
+        int n=heap.size();
+        int left=2*ind+1;
+        int right=2*ind+2;
+        int largest=ind;
+        int value=heap[ind];
+        if(left<n && heap[left]>value){
+            value = heap[left];
+            largest = left;
+        }
+
+        if(right<n && heap[right]>value){
+            value = heap[right];
+            largest = right;
+        }
+
+        if(largest == ind) return;
+        swap(heap[ind],heap[largest]);
+        heapify_down(largest);
+    }
+}
