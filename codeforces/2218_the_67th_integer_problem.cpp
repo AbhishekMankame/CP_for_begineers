087c17345f3c37fb1d98e16c68d7a6591ef3f6e6
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-04-04 18:02:25 +0000

    div 4A - the 67th integer problem

---
+++ codeforces/2218_the_67th_integer_problem.cpp
@@ -0,0 +1,17 @@
+// https://codeforces.com/contest/2218/problem/A
+
+// 2218A - The 67th Integer Problem
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<n<<endl;
+    }
+    return 0;
+}
