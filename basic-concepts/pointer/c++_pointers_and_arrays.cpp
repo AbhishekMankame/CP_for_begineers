9d19ee33c073df605b68aaa9d5a968ea823bc319
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-05-16 17:39:26 +0000

    pointer to every array element

--- basic-concepts/pointer/c++_pointers_and_arrays.cpp
+++ basic-concepts/pointer/c++_pointers_and_arrays.cpp
@@ -24,3 +24,41 @@ ptr = &arr[0];
 The addresses for the rest of the array elements are given by '&arr[1]','&arr[2]','&arr[3]' and '&arr[4]'.
 
 */ 
+
+/*
+### Point to every array element
+
+Suppose we need to point to the fourth element of the array using the same pointer 'ptr'.
+
+Here, if 'ptr' points to the first element in the above example then 'ptr+3' will point to the fourth element. For example,
+
+int *ptr;
+int arr[5];
+ptr = arr;
+
+ptr + 1 is equivalent to &arr[1];
+ptr + 2 is equivalent to &arr[2];
+ptr + 3 is equivalent to &arr[3];
+ptr + 4 is equivalent to &arr[4];
+
+Similarly, we can access the elements using the single pointer. For example,
+
+// use dereference operator
+*ptr == arr[0];
+*(ptr + 1) is equivalent to arr[1];
+*(ptr + 2) is equivalent to arr[2];
+*(ptr + 3) is equivalent to arr[3];
+*(ptr + 4) is equivalent to arr[4];
+
+Suppose if we have initialized 'ptr = &arr[2];' then
+
+ptr - 2 is equivalent to &arr[0];
+ptr - 1 is equivalent to &arr[1]; 
+ptr + 1 is equivalent to &arr[3];
+ptr + 2 is equivalent to &arr[4];
+
+Note: The address between 'ptr' and 'ptr + 1' differs by 4 bytes. It is because 'ptr' is a pointer to an 'int' data. And, the size of int is 4 bytes in a 64-bit operating system.
+
+Similarly, if pointer 'ptr' is pointing to 'char' type data, then the address between 'ptr' and 'ptr+1' is 1 byte. It is because the size of a character is 1 byte.
+
+*/
