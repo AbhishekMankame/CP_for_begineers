81ae9d23eb04b94ef00da86c3c9241eeffa422e9
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-05-18 18:09:04 +0000

    passing 2d

--- basic-concepts/pointer/passing_arrays_to_a_function.cpp
+++ basic-concepts/pointer/passing_arrays_to_a_function.cpp
@@ -60,3 +60,58 @@ void display(int m[])
 Here, we are just expecting an array of integers. C++ handles passing an array to a function in this way to save memory and time.
 
 */
+
+/*
+
+### Passing Multidimensional Array to a Function
+We can also pass Multidimensional arrays as an argument to the function.
+
+Example 2: Passing Multidimensional Array to a Function
+
+*/
+
+#include<iostream>
+using namespace std;
+
+// define a function
+// pass a 2d array as a parameter
+void display(int n[][2]){
+    cout<<"Displaying values: "<<endl;
+    for(int i=0;i<3;i++){
+        for(int j=0;j<2;j++){
+            cout<<"num["<<i<<"]["<<j<<"]: "<<n[i][j]<<endl;
+        }
+    }
+}
+
+int main(){
+    // Initilize 2d array
+    int num[3][2] = {
+        {3,4},{9,5},{7,1}
+    };
+
+    // call the function
+    // pass a 2d array as an argument
+    display(num);
+
+    return 0;
+}
+
+/*
+Output:
+Displaying Values:
+um[0][0]: 3
+num[0][1]: 4
+num[1][0]: 9
+num[1][1]: 5
+num[2][0]: 7
+num[2][1]: 1
+
+In the above program, we have defined a function named 'display()'. The function takes a two dimensional array, 'int n[][2]' as its argument and prints the elements of the array.
+
+While calling the function, we only pass the name of the two dimensional array as the function argument 'display(num)'.
+
+Note: It is not mandatory to specify the number of rows in the array. However, the number of columns should always be specified. This is why we have used 'int n[][2]'.
+
+
+*/
