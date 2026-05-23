56d65c1d559c702da72268864b578b363465d83b
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-05-23 15:22:17 +0000

    c++ string functions

--- basic-concepts/strings/strings_in_c++.cpp
+++ basic-concepts/strings/strings_in_c++.cpp
@@ -174,3 +174,18 @@ int search() {
 }
 
 // Output: "Geeks" found at index: 6
+
+/*
+### C++ string functions
+
+C++ provides some inbuilt functions which are used for string manipulation, such as the strcpy() and strcat() functions for copying and concatenating strings. Some of them are:
+
+length() --> This function returns the length of the string.
+swap() --> This function is used to swap the values of 2 strings.
+size() --> Used to find the size of string
+resize() --> This function is used to resize the length of the string up to the given number of characters.
+push_back() --> This function is used to push the passed character at the end of the string
+pop_back() --> This function is used to pop the last character from the string
+clear() --> This function is used to remove all the elements of the string
+find() --> This function is used to search for a certain substring inside a string and returns the position of the first character of the substring.
+*/
