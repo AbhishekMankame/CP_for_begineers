2fa5c268474eead4fdeb1e1b4b9b3b62ef3ecf14
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-06-04 18:47:49 +0000

    count of frequencyof each element of array

--- simple_programs/basic_programs.cpp
+++ simple_programs/basic_programs.cpp
@@ -180,3 +180,28 @@ int firstAndLastIndex() {
     cout << first << " " << last << endl;
     return 0;
 }
+
+// 9 -  Count of frequency of each element in the array
+
+int frequency() {
+    int arr = {2,3,4,2,5,6,6,6,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    bool vis[] = new bool[n];
+    vis.fill(false);
+
+    for(int i=0;i<n;i++){
+        if(vis[i] == false){
+            int count = 1;
+            vis[i] = true;
+            for(int j=i+1;j<n;j++){
+                if(arr[i] == arr[j]) {
+                    vis[j] = true;
+                    count++;
+                }
+            }
+            cout << arr[i] << "->" << count;
+        }
+    }
+    cout << endl;
+    return 0;
+}
