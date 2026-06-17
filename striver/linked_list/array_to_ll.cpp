6e4ba5340d7aaca47675e738ae1d1c1b86bbc1f2
Abhishek Mankame <61981517+AbhishekMankame@users.noreply.github.com>
2026-06-17 04:25:27 +0000

    remove head in LL

--- striver/linked_list/array_to_ll.cpp
+++ striver/linked_list/array_to_ll.cpp
@@ -18,7 +18,7 @@ class Node {
     }
 };
 
-convertArr2LL(vector<int> &arr) {
+Node* convertArr2LL(vector<int> &arr) {
     Node* head = new Node(arr[0]);
     Node* mover = head;
     for(int i=0;i<arr.size();i++) {
