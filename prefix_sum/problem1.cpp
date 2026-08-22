320957561386c685f9b9cf2878752a946ccb8fd0
Abhishek Mankame <abhishekmankame962@gmail.com>
2026-08-22 15:33:07 +0530

    problem code update

---
+++ prefix_sum/problem1.cpp
@@ -0,0 +1,34 @@
+/*
+Given an array of N elements, answer the following queries:
+Query: L to R
+Result: A[L] + 2*A[L+1] + 3*A[L+2] + ...
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n+1);
+
+    for(int i=1;i<=n;i++) {
+        cin>> v[i];
+    }
+    vector<int> pre1(n,0), pre2(n,0);
+    for(int i=1;i<=n;i++){
+        pre1[i] = pre1[i-1] + (i*v[i]); 
+        pre2[i] = pre2[i-1] + v[i];
+    }
+
+    int q;
+    cin>>q;
+
+    while(q--){
+        int l,r;
+        cin >> l >> r;
+        cout << (pre1[r] - pre1[l-1]) - (l-1)*(pre2[r]-pre2[l-1]) << " ";
+    }
+    cout << endl;
+}
