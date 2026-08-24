d9107bcac38fb08c201eb5f35dbb7e68233e58cd
Abhishek Mankame <abhishekmankame962@gmail.com>
2026-08-24 23:35:43 +0530

    coordinate compression introduction

--- basic-concepts/prefix_sum/coordinate_compression.cpp
+++ basic-concepts/prefix_sum/coordinate_compression.cpp
@@ -1,1 +1,19 @@
-// Coordinate Compression
+/* Coordinate Compression
+
+Coordinate compression is a technique used in DSA/CP to replace large or sparse values with smaller consecutive indices while preserving their relative ordering.
+
+For example,
+
+Original: 100, 5000, 20, 999999
+Compressed: 1, 2, 0, 3
+
+Here, the actual values are replaced by their rank after sorting:
+20 --> 0
+100 --> 1
+5000 --> 2
+999999 --> 3
+
+Simple Definition to remember: Coordinate compression converts large values into smaller indices while maintaining their relative order, allowing us to efficiently use arrays and data structures like Fenwick Trees and Segment Trees.
+
+
+*/
