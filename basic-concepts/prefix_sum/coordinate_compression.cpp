e454aed338dddf2aedad39018998769fca391ff0
Abhishek Mankame <abhishekmankame962@gmail.com>
2026-08-25 23:30:34 +0530

    comments update

--- basic-concepts/prefix_sum/coordinate_compression.cpp
+++ basic-concepts/prefix_sum/coordinate_compression.cpp
@@ -15,7 +15,6 @@ Here, the actual values are replaced by their rank after sorting:
 
 Simple Definition to remember: Coordinate compression converts large values into smaller indices while maintaining their relative order, allowing us to efficiently use arrays and data structures like Fenwick Trees and Segment Trees.
 
-
 */
 
 #include<bits/stdc++.h>
