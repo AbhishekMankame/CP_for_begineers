6fcf00cb8254d0ee8db0dbcdf2b0ff79a5529167
Abhishek Mankame <abhishekmankame962@gmail.com>
2026-08-27 23:27:11 +0530

    complexity and note update

--- prefix_sum/525-Continuous_array_1.cpp
+++ prefix_sum/525-Continuous_array_1.cpp
@@ -30,3 +30,22 @@ public:
     }
 
 };
+
+/*
+Time Complexity:
+There are two nested loops:
+    - Outer loop runs `n` times.
+    - For each `i`, the inner loop runs `n` times.
+
+So roughly: n + (n-1) + (n-2) + ... + 1
+
+This is: n(n+1)/2
+
+Ignoring constants: O(n^2)
+So the time complexity is O(n^2)
+
+Space Complexity: O(1)
+
+Note: This approach with O(n^2) time complexity - results in TLE for larger test cases.
+
+*/
