c2dfd7243829c447fd5cc166457273ae235b25cb
Abhishek Mankame <abhishekmankame962@gmail.com>
2026-08-30 19:34:19 +0530

    complexity update

--- prefix_sum/525-Continuous_array_2.cpp
+++ prefix_sum/525-Continuous_array_2.cpp
@@ -10,12 +10,12 @@ int solve() {
     int n;
     cin >> n;
     vector<int> v(n);
-    for(int &i:v) cin >> i;
+    for(int &i:v) cin >> i; // TC: O(n)
     map<int, int> mp; // A particular sum occured for the first time at which index
     int ans = 0;
     int sum = 0;
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) { // TC: O(n log n)
         if(v[i] == 0) sum -= 1;
         else sum += 1; 
         if(sum == 0) ans = i+1;
@@ -27,7 +27,8 @@ int solve() {
                 mp[sum] = i;
             }
         }
-        return ans;
     }
-
+    return ans;
 }
+
+// Time Complexity: O(n log n)
